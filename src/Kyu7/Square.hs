744d8bb06311e307e64075bc3e13735dd0566bbe
tbsklg <tbs.klg@gmail.com>
2021-08-23 10:32:25 +0200

    Solve 54c27a33fb7da0db0100040e: You are a square

---
+++ src/Kyu7/Square.hs
@@ -0,0 +1,13 @@
+module Kyu7.Square where
+
+isSquare :: Integral n => n -> Bool
+isSquare n
+    | n < 0 = False
+    | n == 0 = True
+    | otherwise = isSquare n n
+    where 
+        isSquare n 0 = False
+        isSquare n counter = counter ^ 2 == n || isSquare n (counter - 1)
+
+isSquare n = square * square == n
+    where square = floor $ sqrt $ (fromIntegral n::Double)
---
+++ test/Kyu7/SquareSpec.hs
@@ -0,0 +1,18 @@
+module Kyu7.SquareSpec where
+
+import Kyu7.Square (isSquare)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "isSquare" $ do
+    it "should work for some examples" $ do
+      isSquare (-1) `shouldBe` False
+      isSquare  0   `shouldBe` True
+      isSquare  3   `shouldBe` False
+      isSquare  4   `shouldBe` True
+      isSquare 25   `shouldBe` True
+      isSquare 26   `shouldBe` False    
+    it "should work for random square numbers" $ do
+      property $ \n -> isSquare (n^2 :: Integer) `shouldBe` True
