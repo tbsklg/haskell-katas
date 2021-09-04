585172ffc210a606d4347ca5bd7bee1ea72601ba
tbsklg <tbs.klg@gmail.com>
2021-09-04 09:17:56 +0200

    Solve 5700c9acc1555755be00027e: Rotations

---
+++ src/Kyu7/Rotations.hs
@@ -0,0 +1,14 @@
+module Kyu7.Rotations where
+
+import Data.List
+
+containAllRots :: String -> [String] -> Bool
+containAllRots str arr = rotate str (length str)
+    where
+        rotate _ 0 = True
+        rotate perm times = permutation `elem` arr && rotate permutation (times - 1)
+            where 
+                permutation = rotateOnce perm
+
+rotateOnce :: String -> String 
+rotateOnce str = take (length str) (drop (length str - 1) (cycle str))
