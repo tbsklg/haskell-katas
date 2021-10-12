7261751b6450bde74a50d8162b87b7bb31367e61
tbsklg <tbs.klg@gmail.com>
2021-10-12 20:34:29 +0200

    Solve 5a58d889880385c2f40000aa: Automorphic Number (Special Numbers Series #6)

---
+++ src/Kyu7/Automorphic.hs
@@ -0,0 +1,19 @@
+module Kyu7.Automorphic where
+
+automorphic :: Integer -> String
+automorphic i
+  | (fromIntegral i ^ 2 - i) `rem` 10 ^ numberOfDigits i == 0 = automorphic'
+  | otherwise = not'
+
+automorphic' :: [Char]
+automorphic' = "Automorphic"    
+
+not' :: [Char]
+not' = "Not!!"
+
+numberOfDigits :: (Ord a, Num a, Show a) => a -> Int
+numberOfDigits n
+  | n < 0 = length (show n) - 1
+  | otherwise = length (show n)
+
+-- automorphic n = bool "Not!!" "Automorphic" $ show n `isSuffixOf` show (n^2)
---
+++ test/Kyu7/AutomorphicSpec.hs
@@ -0,0 +1,19 @@
+module Kyu7.AutomorphicSpec where
+
+import Test.Hspec
+import Kyu7.Automorphic
+
+spec :: Spec
+spec = do
+  it "works on a few fixed cases " $ do
+    automorphic 1 `shouldBe` "Automorphic"
+    automorphic 3 `shouldBe` "Not!!"
+    automorphic 6 `shouldBe` "Automorphic"
+    automorphic 9 `shouldBe` "Not!!"
+    automorphic 25 `shouldBe` "Automorphic"
+    automorphic 53 `shouldBe` "Not!!"
+    automorphic 76 `shouldBe` "Automorphic"
+    automorphic 95 `shouldBe` "Not!!"
+    automorphic 625 `shouldBe` "Automorphic"
+    automorphic 225 `shouldBe` "Not!!"
+    automorphic 1 `shouldBe` "Automorphic"
