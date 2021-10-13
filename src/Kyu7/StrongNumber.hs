86188b4e3ffb4bfce183becc4997b57bb838dc0c
tbsklg <tbs.klg@gmail.com>
2021-10-13 20:34:37 +0200

    Solve 5a4d303f880385399b000001: Strong Number (Special Numbers Series #2)

---
+++ src/Kyu7/StrongNumber.hs
@@ -0,0 +1,23 @@
+module Kyu7.StrongNumber (strong) where
+
+strong :: Integer -> String
+strong i
+  | calcFactSum i == i = "STRONG!!!!"
+  | otherwise = "Not Strong !!"
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1 .. n]
+
+calcFactSum :: Integer -> Integer
+calcFactSum t = for $ digits t
+  where
+    for [] = 0
+    for (x : xs) = factorial x + for xs
+
+digits :: Integral x => x -> [x]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `mod` 10]
+
+-- strong n 
+--   | n == (sum $ map (\d -> product [1..digitToInt d]) $ show n) = "STRONG!!!!"
+--   | otherwise = "Not Strong !!"
---
+++ test/Kyu7/StrongNumberSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.StrongNumberSpec (spec) where
+
+import Kyu7.StrongNumber (strong)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Strong Number" $ do
+    it "Example Tests" $ do
+      strong 1 `shouldBe` "STRONG!!!!"
+      strong 2 `shouldBe` "STRONG!!!!"
+      strong 145 `shouldBe` "STRONG!!!!"
+    --   strong 7 `shouldBe` "Not Strong !!"
+    --   strong 93 `shouldBe` "Not Strong !!"
+    --   strong 185 `shouldBe` "Not Strong !!"
