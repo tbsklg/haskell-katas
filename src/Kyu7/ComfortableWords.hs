33e979c70a4daa9f020a614a47f8ebb10b5b9e76
tbsklg <tbs.klg@gmail.com>
2021-10-17 17:16:53 +0200

    Update 56684677dc75e3de2500002b: Comfortable words

--- src/Kyu7/ComfortableWords.hs
+++ src/Kyu7/ComfortableWords.hs
@@ -2,10 +2,16 @@ module Kyu7.ComfortableWords (comfortableWord) where
 
 comfortableWord :: String -> Bool
 comfortableWord [] = True
-comfortableWord (x:xs)
-    | x `elem` left = foldr ((&&) . (`elem` left)) True (odds (x:xs)) && foldr ((&&) . (`elem` right)) True (evens (x : xs))
-    | x `elem` right = foldr ((&&) . (`elem` right)) True (odds (x : xs)) && foldr ((&&) . (`elem` left)) True (evens (x : xs))
-    | otherwise = False
+comfortableWord (x : xs)
+  | x `elem` left = isComfortableForLeft (odds (x : xs)) && isComfortableForRight (evens (x : xs))
+  | x `elem` right = isComfortableForRight (odds (x : xs)) && isComfortableForLeft (evens (x : xs))
+  | otherwise = False
+
+isComfortableForLeft :: Foldable t => t Char -> Bool
+isComfortableForLeft = foldr ((&&) . (`elem` left)) True
+
+isComfortableForRight :: Foldable t => t Char -> Bool
+isComfortableForRight = foldr ((&&) . (`elem` right)) True
 
 odds :: [a] -> [a]
 odds = fst . split
@@ -19,7 +25,7 @@ left = "qwertasdfgzxcvb"
 right :: [Char]
 right = "yuiophjklnm"
 
-split :: [a] -> ([a],[a])
-split = foldr (\x ~(y2,y1) -> (x:y1, y2)) ([],[])
+split :: [a] -> ([a], [a])
+split = foldr (\x ~(y2, y1) -> (x : y1, y2)) ([], [])
 
 -- comfortableWord = and . (zipWith (/=)  <*> tail) . map (`elem` "yuiophjklnm")
