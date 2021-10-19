12e40495f37fdad4311933a829a31df76d7a85c9
tbsklg <tbs.klg@gmail.com>
2021-10-19 22:02:28 +0200

    Solve 5a54e796b3bfa8932c0000ed: Jumping Number (Special Numbers Series #4)

---
+++ src/Kyu7/Jumping.hs
@@ -0,0 +1,15 @@
+module Kyu7.Jumping where
+
+import Data.Char (digitToInt)
+
+jumpingNumber :: Int -> String
+jumpingNumber d
+  | isJumping . digits $ d = "Jumping!!"
+  | otherwise = "Not!!"
+  where
+    isJumping [] = True
+    isJumping [x] = True
+    isJumping (x : y : ys) = (x + 1 == y || x - 1 == y) && isJumping (y : ys)
+
+digits :: Show a => a -> [Int]
+digits = map digitToInt . show
---
+++ test/Kyu7/JumpingSpec.hs
@@ -0,0 +1,18 @@
+module Kyu7.JumpingSpec where
+
+import Test.Hspec
+import Kyu7.Jumping
+
+spec :: Spec
+spec = do
+   it "finds the jumps" $ do
+      jumpingNumber 1 `shouldBe` "Jumping!!"
+      jumpingNumber 7 `shouldBe` "Jumping!!"
+      jumpingNumber 9 `shouldBe` "Jumping!!"
+      jumpingNumber 23 `shouldBe` "Jumping!!"
+      jumpingNumber 32 `shouldBe` "Jumping!!"
+      jumpingNumber 79 `shouldBe` "Not!!"
+      jumpingNumber 98 `shouldBe` "Jumping!!"
+      jumpingNumber 8987 `shouldBe` "Jumping!!"
+      jumpingNumber 4343456 `shouldBe` "Jumping!!"
+      jumpingNumber 98789876 `shouldBe` "Jumping!!" 
