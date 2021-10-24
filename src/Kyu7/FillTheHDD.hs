263a1ee2de5c65e93a7abef1c9d16b95134131e9
tbsklg <tbs.klg@gmail.com>
2021-10-24 20:21:46 +0200

    Solve 5d49c93d089c6e000ff8428c: Computer problem series #1: Fill the Hard Disk Drive

---
+++ src/Kyu7/FillTheHDD.hs
@@ -0,0 +1,4 @@
+module Kyu7.FillTheHDD (save) where
+
+save :: (Num a,Ord a) => [a] -> a -> Int
+save s c = length . takeWhile (<= c) $ scanl1 (+) s
---
+++ test/Kyu7/FillTheHDDSpec.hs
@@ -0,0 +1,16 @@
+module Kyu7.FillTheHDDSpec (spec) where
+
+import Test.Hspec
+import Kyu7.FillTheHDD (save)
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    save [4, 4, 4, 3, 3] 12 `shouldBe` 3
+    save [4, 4, 4, 3, 3] 11 `shouldBe` 2
+    save [4, 8, 15, 16, 23, 42] 108 `shouldBe` 6
+    save [13] 13 `shouldBe` 1
+    save [1, 2, 3, 4] 250 `shouldBe` 4
+    save [100] 500 `shouldBe` 1
+    save [11, 13, 15, 17, 19] 8 `shouldBe` 0
+    save [45] 12 `shouldBe` 0
