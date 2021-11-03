309e18ea0a58e1355456ab23995aea2cfa686662
tbsklg <tbs.klg@gmail.com>
2021-11-03 13:25:57 +0100

    Solve 591592b0f05d9a3019000087: Simple Fun #274: Diplomas

---
+++ src/Kyu7/Diplomas.hs
@@ -0,0 +1,10 @@
+module Kyu7.Diplomas where
+
+diplomas :: Int -> Int -> Int -> Int
+diplomas h w n = maxDiplomas h w n 1
+
+maxDiplomas :: Integral t => t -> t -> t -> t -> t
+maxDiplomas _ _ 0 _ = 0
+maxDiplomas h w n s 
+  | (s `div` h) * (s `div` w) >= n = s
+  | otherwise = maxDiplomas h w n (s + 1)
---
+++ test/Kyu7/DiplomasSpec.hs
@@ -0,0 +1,19 @@
+module Kyu7.DiplomasSpec where
+
+import Test.Hspec
+import Kyu7.Diplomas
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  it "works on fixed cases" $ do
+    diplomas 2 3 10       `shouldBe` 9
+    diplomas 1 1 1        `shouldBe` 1
+    diplomas 567 120 129  `shouldBe` 3120
+    diplomas 17 21 0      `shouldBe` 0
+    diplomas 1 239 7      `shouldBe` 239
+    diplomas 239 239 7    `shouldBe` 717
+    diplomas 3 4 12       `shouldBe` 12
+    
+-- the following line is optional for 8.2
+main = hspec spec
