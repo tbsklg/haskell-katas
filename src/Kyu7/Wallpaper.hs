c32c62fe7349b7666ec31a7aa12720ae2f23cdaa
tbsklg <tbs.klg@gmail.com>
2021-11-07 08:29:10 +0100

    Solve Easy 567501aec64b81e252000003: Easy wallpaper

---
+++ src/Kyu7/Wallpaper.hs
@@ -0,0 +1,14 @@
+module Kyu7.Wallpaper where
+
+wallpaper :: Double -> Double -> Double -> String
+wallpaper l w h = numbers !! numberOfRollsToBuy
+  where
+    numberOfRollsToBuy 
+        | l == 0 || w == 0 || h == 0 = 0
+        | otherwise = ceiling $ (l * h * 2 + w * h * 2) * 1.15 / standardRoll
+
+standardRoll :: Double
+standardRoll = 0.52 * 10.0
+
+numbers :: [[Char]]
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
---
+++ test/Kyu7/WallpaperSpec.hs
@@ -0,0 +1,19 @@
+module Kyu7.WallpaperSpec where
+import Kyu7.Wallpaper (wallpaper)  
+
+import Test.Hspec
+import Test.QuickCheck
+import Text.Printf (printf)
+
+testWallpaper :: Double -> Double -> Double -> String -> Spec
+testWallpaper l w h u = 
+    it (printf "should return wallpaper for l : %f w : %f h : %f " l w h) $
+        wallpaper l w h `shouldBe` u
+
+spec :: Spec
+spec = do
+    describe "wallpaper" $ do
+        testWallpaper 6.3 4.5 3.29 "sixteen"
+        testWallpaper 7.8 2.9 3.29 "sixteen"
+        testWallpaper 6.3 5.8 3.13 "seventeen"
+        testWallpaper 6.1 6.7 2.81 "sixteen"
