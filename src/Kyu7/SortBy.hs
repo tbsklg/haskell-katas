3616447bbfb1d0aeb0be031fb4b371ae27eda1df
tbsklg <tbs.klg@gmail.com>
2021-11-09 10:49:59 +0100

    Solve 57eba158e8ca2c8aba0002a0: Sort by Last Char

---
+++ src/Kyu7/SortBy.hs
@@ -0,0 +1,13 @@
+module Kyu7.SortBy where
+
+import Data.List (sortBy)
+
+last' :: String -> [String]
+last' = sortBy lastChar . words
+
+lastChar :: Ord a => [a] -> [a] -> Ordering
+lastChar a b
+  | last a <= last b = LT
+  | otherwise = GT
+
+-- last' = sortOn last . words
---
+++ test/Kyu7/SortBySpec.hs
@@ -0,0 +1,14 @@
+module Kyu7.SortBySpec where
+
+import Test.Hspec
+import Kyu7.SortBy
+
+spec :: Spec
+spec = do
+    describe "sorts a string into a list of words by the last character" $ do
+        it "a few fixed cases" $ do
+        last' "man i need a taxi up to ubud" `shouldBe` ["a", "need", "ubud", "i", "taxi", "man", "to", "up"]
+        last' "what time are we climbing up the volcano" `shouldBe` ["time", "are", "we", "the", "climbing", "volcano", "up", "what"]
+        last' "take me to semynak" `shouldBe` ["take", "me", "semynak", "to"]
+        last' "massage yes massage yes massage" `shouldBe` ["massage", "massage", "massage", "yes", "yes"]
+        last' "take bintang and a dance please" `shouldBe` ["a", "and", "take", "dance", "please", "bintang"]
