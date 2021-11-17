6ab6be70b493f845fff5a78d3d17f54e51cfadfe
tbsklg <tbs.klg@gmail.com>
2021-11-17 15:22:21 +0100

    Solve 569218bc919ccba77000000b: Target Date

---
+++ src/Kyu7/Dateamount.hs
@@ -0,0 +1,11 @@
+module Kyu7.Dateamount where
+
+import Data.Time
+import Data.Time.Format
+
+dateNbDays :: Double -> Double -> Double -> String
+dateNbDays a0 a p = calcDays a0 (fromGregorian 2016 01 01)
+    where
+        calcDays current d  
+            | current < a = calcDays (current + current * (p / 36000)) (addDays 1 d)
+            | otherwise = show d
