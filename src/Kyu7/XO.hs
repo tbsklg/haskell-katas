4a514b2ca0b6159ea5e445deccf7c6ba066d038e
tbsklg <tbs.klg@gmail.com>
2021-11-18 10:24:01 +0100

    Solve 55908aad6620c066bc00002a: Exes and Ohs

---
+++ src/Kyu7/XO.hs
@@ -0,0 +1,14 @@
+module Kyu7.XO where
+
+import Data.Char (toLower)
+import Data.List (partition)
+
+-- | Returns true if the number of
+-- Xs is equal to the number of Os
+-- (case-insensitive)
+xo :: String -> Bool
+xo str = xLength == oLength
+  where
+    xLength = length . filter (=='x') $ asLowerCase
+    oLength = length . filter (=='o') $ asLowerCase
+    asLowerCase = map toLower str
---
+++ test/Kyu7/XOSpec.hs
@@ -0,0 +1,29 @@
+module Kyu7.XOSpec where
+
+import Kyu7.XO (xo)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "xo" $ do
+    it "should work on some examples" $ do
+      expectTrue "xo"
+      expectTrue "Xo"
+      expectTrue "xxOo"
+      expectFalse "xxxm"
+      expectFalse "Oo"
+      expectFalse "ooom"
+    
+    it "should return true for strings that don't contain x or o" $ property $
+      forAll (filter (\k -> k `notElem` "xXoO") `fmap` arbitrary) $ \str ->
+        expectTrue str      
+      
+expectFalse, expectTrue :: String -> Expectation
+expectTrue s 
+  | xo s       = return () 
+  | otherwise  = expectationFailure $ "Expected True but got False on " ++ show s
+
+expectFalse s 
+  | not $ xo s = return () 
+  | otherwise  = expectationFailure $ "Expected False but got True on " ++ show s
