a5567b9c5ddc78f7d4e6160823921468952ae550
tbsklg <tbs.klg@gmail.com>
2021-11-19 16:41:20 +0100

    Solve 5ad29cd95e8240dd85000b54: Friends

---
+++ src/Kyu7/Friends.hs
@@ -0,0 +1,12 @@
+module Kyu7.Friends where
+
+friends :: Int -> Int
+friends 0 = 0
+friends 1 = 0
+friends i
+    | isInt logBase2 = floor (logBase2 - 1)
+    | otherwise = floor logBase2
+    where
+        logBase2 = logBase 2 $ (fromIntegral i)
+
+isInt x = x == fromInteger (round x)
