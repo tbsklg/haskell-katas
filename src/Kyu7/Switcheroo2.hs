39582fda6412ddd6cd47b323f83459b286740542
tbsklg <tbs.klg@gmail.com>
2021-11-25 17:10:18 +0100

    Solve 55d410c492e6ed767000004f: The old switcheroo

---
+++ src/Kyu7/Switcheroo2.hs
@@ -0,0 +1,7 @@
+module Kyu7.Switcheroo2 where
+
+vowel2Index :: String -> String
+vowel2Index =concatMap (\x -> if snd x `elem` vowels then show (fst x) else [snd x]) . zip [1 ..]
+
+vowels :: [Char]
+vowels = "AEIOUaeiou"
