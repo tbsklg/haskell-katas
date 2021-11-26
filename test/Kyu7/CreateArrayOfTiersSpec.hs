8d4676cc52d13526661d3cbde1eed959e149d76f
tbsklg <tbs.klg@gmail.com>
2021-11-26 16:30:20 +0100

    Solve 586bca7fa44cfc833e00005c: Number to digit tiers

---
+++ test/Kyu7/CreateArrayOfTiersSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.CreateArrayOfTiersSpec (spec) where
+
+import Test.Hspec
+import Kyu7.CreateArrayOfTiers (createArrayOfTiers)
+
+spec :: Spec
+spec = do
+  it "fixed tests" $ do
+    createArrayOfTiers 0 `shouldBe` ["0"]
+    createArrayOfTiers 6 `shouldBe` ["6"]
+    createArrayOfTiers 420 `shouldBe` ["4", "42", "420"]
+    createArrayOfTiers 2017 `shouldBe` ["2", "20", "201", "2017"]
+    createArrayOfTiers 2010 `shouldBe` ["2", "20", "201", "2010"]
+    createArrayOfTiers 4020 `shouldBe` ["4", "40", "402", "4020"]
+    createArrayOfTiers 80200 `shouldBe` ["8", "80", "802", "8020", "80200"]
