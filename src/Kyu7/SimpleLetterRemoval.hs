a3fa12c81077acb7af17e6371bae31b10e7cfb5a
tbsklg <tbs.klg@gmail.com>
2021-11-27 13:51:25 +0100

    Solve 5b728f801db5cec7320000c7: Simple letter removal

---
+++ src/Kyu7/SimpleLetterRemoval.hs
@@ -0,0 +1,18 @@
+module Kyu7.SimpleLetterRemoval where
+
+import Data.List (elemIndex)
+
+solve :: [Char] -> Int -> [Char]
+solve l k = remove' l k alphabet
+  where
+    remove' [] _ _ = []
+    remove' l 0 _ = l
+    remove' l k a
+      | k > length l = []
+      | otherwise =
+        case elemIndex (head a) l of
+          Just a -> remove' (take a l ++ drop (a + 1) l) (k - 1) alphabet
+          Nothing -> remove' l k (tail a)
+
+alphabet :: [Char]
+alphabet = ['a' .. 'z']
---
+++ test/Kyu7/SimpleLetterRemovalSpec.hs
@@ -0,0 +1,14 @@
+module Kyu7.SimpleLetterRemovalSpec where
+import Kyu7.SimpleLetterRemoval
+import Test.Hspec
+
+spec = do
+  describe "Simple letter removal" $ do
+    it "Basic tests" $ do
+      solve "abracadabra" 0 `shouldBe` "abracadabra"
+      solve "abracadabra" 1 `shouldBe` "bracadabra"
+      solve "abracadabra" 2 `shouldBe` "brcadabra"
+      solve "abracadabra" 6 `shouldBe` "rcdbr"
+      solve "abracadabra" 8 `shouldBe` "rdr"
+      solve "abracadabra" 50 `shouldBe` ""
+      solve "u" 1 `shouldBe` ""
