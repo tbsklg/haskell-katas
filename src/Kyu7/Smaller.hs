8983af2e274135153f9a2c6ee52ee6606092369f
tbsklg <tbs.klg@gmail.com>
2021-12-01 20:21:13 +0100

    Solve 56a1c074f87bc2201200002e: How many are smaller than me?

---
+++ src/Kyu7/Smaller.hs
@@ -0,0 +1,5 @@
+module Kyu7.Smaller where
+
+smaller :: Ord a => [a] -> [Int]
+smaller [] = []
+smaller (x:xs) = (length . filter (<x) $ xs) : smaller xs
---
+++ test/Kyu7/SmallerSpec.hs
@@ -0,0 +1,13 @@
+module Kyu7.SmallerSpec where
+
+import Kyu7.Smaller (smaller)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "smaller" $ do
+    it "works for some examples" $ do
+      smaller [5,4,3,2,1]  `shouldBe` [4,3,2,1,0]
+      smaller [1,2,3]      `shouldBe` [0,0,0]
+      smaller ([] :: [()]) `shouldBe` []
