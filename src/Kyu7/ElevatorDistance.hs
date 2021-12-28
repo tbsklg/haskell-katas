418925043eef03db643f58a0d2b284113ce42842
tbsklg <tbs.klg@gmail.com>
2021-12-28 13:26:48 +0100

    Solve 59f061773e532d0c87000d16: Elevator Distance

---
+++ src/Kyu7/ElevatorDistance.hs
@@ -0,0 +1,6 @@
+module Kyu7.ElevatorDistance (elevatorDistance) where
+
+elevatorDistance :: [Int] -> Int
+elevatorDistance [] = 0
+elevatorDistance [x] = 0
+elevatorDistance (x:y:ys) = abs(x - y) + elevatorDistance (y:ys)
---
+++ test/Kyu7/ElevatorDistanceSpec.hs
@@ -0,0 +1,11 @@
+module Kyu7.ElevatorDistanceSpec (spec) where
+
+import Kyu7.ElevatorDistance (elevatorDistance)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    elevatorDistance [5,2,8] `shouldBe` 9
+    elevatorDistance [1,2,3] `shouldBe` 2
+    elevatorDistance [7,1,7,1] `shouldBe` 18
