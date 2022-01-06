6963d2297c978a75872191a60bb8612c811bb2af
tbsklg <tbs.klg@gmail.com>
2022-01-06 18:46:04 +0100

    Solve 5a4ea304b3bfa89a9900008e: Form The Largest

---
+++ src/Kyu7/Largest2.hs
@@ -0,0 +1,6 @@
+module Kyu7.Largest2 where
+
+import Data.List (sort)
+
+maxNumber :: Int -> Int
+maxNumber i =  read (concatMap show . reverse . sort . map (\x -> read [x] :: Int ) . show $ i) :: Int
---
+++ test/Kyu7/Largest2Spec.hs
@@ -0,0 +1,27 @@
+module Kyu7.Largest2Spec where
+
+import Test.Hspec
+import Kyu7.Largest2
+
+import Test.HUnit (assertEqual)
+
+myTest n exp = do
+  assertEqual msg exp actual
+  where 
+    actual = maxNumber n
+    msg = "Failed at: " ++ showArgs
+    showArgs = show n
+
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    myTest 213      321
+    myTest 7389     9873
+    myTest 63792    97632
+    myTest 566797   977665
+    myTest 1000000  1000000
+
+-- the following line is optional for 8.2
+main = hspec spec
