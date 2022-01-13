cfc158f323c4917e466ef6cc33ba5d5e7fe33651
tbsklg <tbs.klg@gmail.com>
2022-01-13 20:41:49 +0100

    Solve 59cfc000aeb2844d16000075: Alternate capitalization

---
+++ src/Kyu7/Capitalize.hs
@@ -0,0 +1,8 @@
+module Kyu7.Capitalize where
+import Data.Char
+
+capitalize :: [Char] -> [[Char]]
+capitalize xs = evenUp : [oddUp]
+    where
+        evenUp = zipWith (\ x y -> (if even x then toUpper y else y)) [0..] xs
+        oddUp = zipWith (\ x y -> (if odd x then toUpper y else y)) [0..] xs
---
+++ test/Kyu7/CapitalizeSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.CapitalizeSpec (spec, main) where
+
+import Kyu7.Capitalize
+import Test.Hspec
+
+main = hspec spec
+spec = do
+  describe "Alternate capitalization" $ do
+    it "should work for simple examples" $ do
+      capitalize "abcdef" `shouldBe` ["AbCdEf", "aBcDeF"]
+      capitalize "codewars" `shouldBe` ["CoDeWaRs", "cOdEwArS"]
+      capitalize "abracadabra" `shouldBe` ["AbRaCaDaBrA", "aBrAcAdAbRa"]
+      capitalize "codewarriors" `shouldBe` ["CoDeWaRrIoRs", "cOdEwArRiOrS"]
+      capitalize "indexinglessons" `shouldBe` ["InDeXiNgLeSsOnS", "iNdExInGlEsSoNs"]
+      capitalize "codingisafunactivity" `shouldBe` ["CoDiNgIsAfUnAcTiViTy", "cOdInGiSaFuNaCtIvItY"]
