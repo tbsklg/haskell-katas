64e0b1518669d63a7415aa342b7c33b440dd48f1
tbsklg <tbs.klg@gmail.com>
2022-01-17 22:28:17 +0100

    Update 52829c5fe08baf7edc00122b: Number Of Occurrences

--- src/Kyu7/Occurences.hs
+++ src/Kyu7/Occurences.hs
@@ -3,8 +3,4 @@ module Kyu7.Occurences where
 import Data.List ( group, sort )
 
 numberOfOccurrences :: (Eq a, Ord a) => a -> [a] -> Int
-numberOfOccurrences x xs
-    | not (null occurences) = length . head $ occurences
-    | otherwise = 0
-    where
-        occurences = filter((==) x . head) . group . sort $ xs
+numberOfOccurrences x = length . filter (==x)
