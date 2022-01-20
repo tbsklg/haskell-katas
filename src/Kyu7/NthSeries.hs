1e7d4670e4331faedf723a1505ce443d28388357
tbsklg <tbs.klg@gmail.com>
2022-01-20 08:19:31 +0100

    Solve 555eded1ad94b00403000071: Sum of the first nth term of Series

---
+++ src/Kyu7/NthSeries.hs
@@ -0,0 +1,9 @@
+module Kyu7.NthSeries where
+
+import Text.Printf
+
+series :: [Double]
+series = map (1/) [1, 4 ..]
+
+seriesSum :: Integer -> String
+seriesSum i = printf "%.2f" $ sum $ take (fromInteger i) series
---
+++ test/Kyu7/NthSeriesSpec.hs
@@ -0,0 +1,18 @@
+module Kyu7.NthSeriesSpec where
+
+import Kyu7.NthSeries
+
+import Test.Hspec
+import Text.Printf (printf)
+
+--testSumSeries :: Integer -> String -> SpecWith (Arg Expectation)
+testSumSeries nb r = 
+    it (printf " %i " nb) $
+        seriesSum nb `shouldBe` r
+    
+spec :: Spec
+spec = do
+    describe "Nth series" $ do
+        testSumSeries 0 "0.00"
+        testSumSeries 9 "1.77"
+        testSumSeries 15 "1.94"
