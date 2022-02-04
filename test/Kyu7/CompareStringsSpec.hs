858e71b66cdad54cdae8f0fe85dd07ff4cc56435
tbsklg <tbs.klg@gmail.com>
2022-02-04 12:15:13 +0100

    Solve 576bb3c4b1abc497ec000065: Compare Strings by Sum of Chars

---
+++ test/Kyu7/CompareStringsSpec.hs
@@ -0,0 +1,21 @@
+module Kyu7.CompareStringsSpec (spec) where
+
+import Test.Hspec
+import Kyu7.CompareStrings
+
+main = hspec spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      compare' (Just "AD")   (Just "BC")    `shouldBe` True
+      compare' (Just "AD")   (Just "DD")    `shouldBe` False
+      compare' (Just "gf")   (Just "FG")    `shouldBe` True
+      compare' (Just "Ad")   (Just "DD")    `shouldBe` False
+      compare' (Just "zz1")  (Just "")      `shouldBe` True
+      compare' (Just "ZzZz") (Just "ffPFF") `shouldBe` True
+      compare' (Just "kl")   (Just "lz")    `shouldBe` False
+      compare' Nothing       (Just "")      `shouldBe` True
+      compare' (Just "!!")   (Just "7476")  `shouldBe` True
+      compare' (Just "##")   (Just "1176")  `shouldBe` True
+      compare' (Just "a1")   Nothing        `shouldBe` True
+      compare' Nothing       (Just "a1")    `shouldBe` True
