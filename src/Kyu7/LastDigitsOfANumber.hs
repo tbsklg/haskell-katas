c465e2852cfe256e29b4c58b9ca5f898845fce5c
tbsklg <tbs.klg@gmail.com>
2022-02-06 18:41:28 +0100

    Solve 5cd5ba1ce4471a00256930c0; last digits of a number

---
+++ src/Kyu7/LastDigitsOfANumber.hs
@@ -0,0 +1,9 @@
+module Kyu7.LastDigitsOfANumber (lastDigits) where
+
+lastDigits :: Int -> Int -> [Int]
+lastDigits 0 _ = [0]
+lastDigits t s = reverse . take s . reverse . digits $ t
+
+digits :: Integral x => x -> [x]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `mod` 10]
