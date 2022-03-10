e35e22e62be96f0a7714b00389fd57d5e681543f
tbsklg <tbs.klg@gmail.com>
2022-03-10 11:39:38 +0100

    Update 5641c3f809bf31f008000042: Formatting decimal places #1

--- src/Kyu7/Decimals.hs
+++ src/Kyu7/Decimals.hs
@@ -7,3 +7,5 @@ format :: (RealFrac a1, Fractional a2) => a1 -> a2
 format n
   | n < 0 = fromIntegral (ceiling (n * 10 ^ 2)) / 10 ^ 2
   | otherwise = fromIntegral (floor (n * 10 ^ 2)) / 10 ^ 2
+
+-- twoDecimalPlaces = (/100) . fromInteger . truncate . (*100)
