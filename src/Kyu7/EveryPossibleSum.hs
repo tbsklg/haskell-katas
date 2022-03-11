0d5b5a2f8aca38cb884b7d1dcb5f7fd4364b0670
tbsklg <tbs.klg@gmail.com>
2022-03-11 10:50:00 +0100

    Solve 5b4e474305f04bea11000148: Every possible sum of two digits

---
+++ src/Kyu7/EveryPossibleSum.hs
@@ -0,0 +1,11 @@
+module Kyu7.EveryPossibleSum (digits) where
+
+digits :: (Integral a) => a -> [a]
+digits = go . digits'
+    where
+        go [] = []
+        go (x:xs) = map (x +) xs ++ go xs
+
+digits' :: Integral a => a -> [a]
+digits' 0 = []
+digits' d = digits' (d `div` 10) ++ [d `mod` 10]
---
+++ test/Kyu7/EveryPossibleSumSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.EveryPossibleSumSpec (spec) where
+
+import Test.Hspec
+import Kyu7.EveryPossibleSum (digits)
+
+spec = do
+  describe "Every possible sum of two digits" $ do
+    it "Example tests" $ do
+      digits     156 ?= [ 6, 7, 11 ]
+      digits   81596 ?= [ 9, 13, 17, 14, 6, 10, 7, 14, 11, 15 ]
+      digits    3852 ?= [ 11, 8, 5, 13, 10, 7 ]
+      digits 3264128 ?= [ 5, 9, 7, 4, 5, 11, 8, 6, 3, 4, 10, 10, 7, 8, 14, 5, 6, 12, 3, 9, 10 ]
+      digits  999999 ?= [ 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18 ]
+
+(?=) = shouldBe
