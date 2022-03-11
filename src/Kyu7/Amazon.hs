4e9b489bcc27a4a6455ff6bc4a029086a0247263
tbsklg <tbs.klg@gmail.com>
2022-03-11 12:39:23 +0100

    Solve 55b95c76e08bd5eef100001e: Counting in the Amazon

---
+++ src/Kyu7/Amazon.hs
@@ -0,0 +1,9 @@
+module Kyu7.Amazon where
+
+countArara :: Int -> String
+countArara = unwords . adakOrAnane
+
+adakOrAnane :: Int -> [String]
+adakOrAnane i
+    | even i = replicate (i `div` 2) "adak"
+    | otherwise = adakOrAnane (i - 1) ++ ["anane"]
---
+++ test/Kyu7/AmazonSpec.hs
@@ -0,0 +1,17 @@
+module Kyu7.AmazonSpec where
+
+import Test.Hspec
+import Kyu7.Amazon
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  it "does the amazon counting" $ do
+    countArara 1 `shouldBe` "anane"
+    countArara 2 `shouldBe` "adak"
+    countArara 3 `shouldBe` "adak anane"
+    countArara 9 `shouldBe` "adak adak adak adak anane"
+
+    
+-- the following line is optional for 8.2
+main = hspec spec
