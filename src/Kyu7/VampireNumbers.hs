840df6b0ac7fdae13c9ff6e6137e47c408b6e990
tbsklg <tbs.klg@gmail.com>
2022-03-12 09:36:39 +0100

    Solve 54d418bd099d650fa000032d: Vampire Numbers

---
+++ src/Kyu7/VampireNumbers.hs
@@ -0,0 +1,16 @@
+module Kyu7.VampireNumbers where
+
+import Data.List (sort)
+
+isVampire :: Integer -> Integer -> Bool
+isVampire a b = (a > 0 || b > 0) && sameDigits 
+  where
+    sameDigits = sort (a' ++ b') == sort c'
+    
+    a' = digits a
+    b' = digits b
+    c' = digits (a * b)
+
+digits :: Integral a => a -> [a]
+digits 0 = []
+digits i = digits (abs i `div` 10) ++ [abs i `mod` 10]
