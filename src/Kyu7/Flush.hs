052bbde55877633f07a5efdf2e252b9578cb42f4
tbsklg <tbs.klg@gmail.com>
2022-03-16 08:19:52 +0100

    Solve 5acbc3b3481ebb23a400007d: Determine if the poker hand is flush

---
+++ src/Kyu7/Flush.hs
@@ -0,0 +1,6 @@
+module Kyu7.Flush where
+
+isFlush :: [String] -> Bool
+isFlush xs = all ((== suit) . last) xs
+    where
+        suit = last . head $ xs
---
+++ test/Kyu7/FlushSpec.hs
@@ -0,0 +1,19 @@
+module Kyu7.FlushSpec  where
+-- Tests can be written using Hspec http://hspec.github.io/
+-- Replace this with your own tests.
+
+import Test.Hspec
+import Kyu7.Flush
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  describe "checks for flush" $ do
+    it "a few fixed cases" $ do
+      isFlush (["AS", "3S", "9S", "KS", "4S"]) `shouldBe`  True
+      isFlush (["AD", "4S", "7H", "KC", "5S"]) `shouldBe`  False
+      isFlush (["AD", "4S", "10H", "KC", "5S"]) `shouldBe` False
+      isFlush (["QD", "4D", "10D", "KD", "5D"]) `shouldBe` True
+  
+-- the following line is optional for 8.2
+main = hspec spec
