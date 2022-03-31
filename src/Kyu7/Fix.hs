68bdd60e82279acf30cc3a67c9567bd90309a46e
tbsklg <tbs.klg@gmail.com>
2022-03-31 15:28:32 +0200

    Fix 5bf774a81505a7413400006a: Sentences should start with capital letters.

--- src/Kyu7/Fix.hs
+++ src/Kyu7/Fix.hs
@@ -5,4 +5,5 @@ import Data.List (intercalate)
 import Data.List.Split (splitOn)
 
 fix' :: String -> String
-fix' = intercalate ". " . map (\x -> (:) (toUpper . head $ x) . tail $ x) . splitOn ". "
+fix' "" = ""
+fix' xs = intercalate ". " . map (\x -> (:) (toUpper . head $ x) . tail $ x) . splitOn ". " $ xs
