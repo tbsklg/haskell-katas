2e56a5abbc3508358c05f39cfc3b32ea400806de
tbsklg <tbs.klg@gmail.com>
2022-04-01 09:42:24 +0200

    Solve 561046a9f629a8aac000001d: Array comparator

---
+++ src/Kyu7/Comparator.hs
@@ -0,0 +1,4 @@
+module Kyu7.Comparator where
+
+matchList :: Eq a => [a] -> [a] -> Int
+matchList xs ys = length [x | x <- xs, x `elem` ys]
---
+++ test/Kyu7/ComparatorSpec.hs
@@ -0,0 +1,10 @@
+module Kyu7.ComparatorSpec (spec) where
+
+import Kyu7.Comparator (matchList)
+import Test.Hspec
+import Test.QuickCheck
+
+spec = do
+  it "works for some examples" $ do
+    matchList ["Erlang", "Javascript"]  ["Go", "C++", "Haskell"] `shouldBe`  0
+    matchList ["Haskell", "Javascript"] ["Go", "C++", "Haskell"] `shouldBe`  1
