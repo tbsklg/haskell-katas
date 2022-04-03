b995a23668ef20c7556f0cb6ea77ad7a3667393f
tbsklg <tbs.klg@gmail.com>
2022-04-03 08:42:19 +0200

    Solve 55805ab490c73741b7000064: makeBackronym

---
+++ src/Kyu7/Backronym.hs
@@ -0,0 +1,39 @@
+module Kyu7.Backronym where
+
+import Data.Maybe (fromJust)
+import Data.Char (toUpper)
+
+dict :: Char -> String
+dict 'A' = "awesome"
+dict 'B' = "beautiful"
+dict 'C' = "confident"
+dict 'D' = "disturbing"
+dict 'E' = "eager"
+dict 'F' = "fantastic"
+dict 'G' = "gregarious"
+dict 'H' = "hippy"
+dict 'I' = "ingestable"
+dict 'J' = "joke"
+dict 'K' = "klingon"
+dict 'L' = "literal"
+dict 'M' = "mustache"
+dict 'N' = "newtonian"
+dict 'O' = "oscillating"
+dict 'P' = "perfect"
+dict 'Q' = "queen"
+dict 'R' = "rant"
+dict 'S' = "stylish"
+dict 'T' = "turn"
+dict 'U' = "underlying"
+dict 'V' = "volcano"
+dict 'W' = "weird"
+dict 'X' = "xylophone"
+dict 'Y' = "yogic"
+dict 'Z' = "zero"
+dict _ = ""
+
+-- dict :: Char -> String
+-- Can only map uppercase letters.
+
+makeBackronym :: String -> String
+makeBackronym = unwords . map (dict . toUpper)
---
+++ test/Kyu7/BackronymSpec.hs
@@ -0,0 +1,25 @@
+module Kyu7.BackronymSpec where
+
+import Kyu7.Backronym (makeBackronym)
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "makeBackronym" $ do
+    it "works on an empty string" $ makeBackronym "" `shouldBe` ""
+    
+    it "works on examples" $ do
+      makeBackronym "dgm" `shouldBe` 
+        "disturbing gregarious mustache"
+      makeBackronym "lkj" `shouldBe` 
+        "literal klingon joke"
+      makeBackronym "interesting" `shouldBe` 
+        "ingestable newtonian turn eager rant eager stylish turn ingestable newtonian gregarious"
+      makeBackronym "codewars" `shouldBe`
+        "confident oscillating disturbing eager weird awesome rant stylish"
+    
+    prop "returns the correct number of words" $
+      forAll (listOf (choose ('a','z'))) $ \word ->
+        makeBackronym word `shouldSatisfy` (length word ==) . length . words
