ee21d679569bf3a7150457fb099cea3658987cb4
tbsklg <tbs.klg@gmail.com>
2022-04-05 21:06:55 +0200

    Solve 596776fbb4f24d0d82000141: Alternating between three values

--- src/Kyu7/ListOfPresents.hs
+++ src/Kyu7/ListOfPresents.hs
@@ -2,8 +2,8 @@ module Kyu7.ListOfPresents (howManyGifts) where
 import Data.List (sort)
 
 howManyGifts :: Int -> [Int] -> Int
-howManyGifts maxBudget = (+ 1)
-    . length
+howManyGifts maxBudget =
+    length
     . takeWhile (<= maxBudget)
     . scanl1 (+)
     . sort
