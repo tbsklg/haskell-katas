f12b8a7dff62c636d4c028665fa8d8bd469ad8f6
tbsklg <tbs.klg@gmail.com>
2022-04-05 09:55:45 +0200

    Solve 58aa68605aab54a26c0001a6: Simple Fun #144: Distinct Digit Year

---
+++ src/Kyu7/DistinctDigitYear.hs
@@ -0,0 +1,13 @@
+module Kyu7.DistinctDigitYear where
+
+import Data.List (nub)
+
+distinctDigitYear :: Int -> Int
+distinctDigitYear x = head . dropWhile (not . isDistinct) $ [(x + 1) .. ]
+
+isDistinct :: Int -> Bool
+isDistinct a = digits a == (nub . digits $ a)
+
+digits :: Integral a => a -> [a]
+digits 0 = []
+digits a = digits (a `div` 10) ++ [a `mod` 10]
---
+++ test/Kyu7/DistinctDigitYearSpec.hs
@@ -0,0 +1,12 @@
+module Kyu7.DistinctDigitYearSpec where
+
+import Test.Hspec
+import Kyu7.DistinctDigitYear
+
+spec :: Spec
+spec = do
+  describe "finds the next distinct year" $ do
+    it "a few fixed cases" $ do
+      distinctDigitYear 1987 `shouldBe` 2013
+      distinctDigitYear 2013 `shouldBe` 2014
+      distinctDigitYear 2229 `shouldBe` 2301 
