8d6d700d94cacfaac3df952358650809ef7a0a3c
tbsklg <tbs.klg@gmail.com>
2022-04-12 09:26:17 +0200

    Solve 5dd5128f16eced000e4c42ba: Most valuable character

---
+++ src/Kyu7/MostValued.hs
@@ -0,0 +1,33 @@
+module Kyu7.MostValued where
+
+import Data.List (elemIndices, groupBy, nub, sort, sortBy, sortOn)
+
+solve :: String -> Char
+solve l
+  | null values = minimum l
+  | otherwise = last values
+  where
+    values =
+      map snd
+        . filter (\(c, _) -> c /= 0)
+        . nub
+        . sortBy mostValuable
+        . map value
+        $ l
+    value x = (distance . elemIndices x $ l, x)
+    distance l = maximum l - minimum l
+
+mostValuable :: (Ord a1, Ord a2) => (a2, a1) -> (a2, a1) -> Ordering
+mostValuable (c, a) (c', b)
+  | c == c' = alpha a b
+  | c < c' = LT
+  | otherwise = GT
+
+alpha :: Ord a => a -> a -> Ordering
+alpha a b 
+    | a < b = GT
+    | otherwise = LT
+
+-- solve :: String -> Char 
+-- solve xs = snd $ minimum $ map charVal xs
+--   where charVal x = (liftA2 (-) head last (elemIndices x xs), x)
---
+++ test/Kyu7/MostValuedSpec.hs
@@ -0,0 +1,13 @@
+module Kyu7.MostValuedSpec where
+
+import Kyu7.MostValued
+import Test.Hspec
+
+spec = do
+  describe "Most Valuable Character" $ do
+    it "Basic tests" $ do
+      solve "a" `shouldBe` 'a'
+      solve "aa" `shouldBe` 'a'
+      solve "bcd" `shouldBe` 'b'
+      solve "axyzxyz" `shouldBe` 'x'
+      solve "aabccc" `shouldBe` 'c' 
