ea9849beb3dc3e1122e9a40a49831ea91fccee24
tbsklg <tbs.klg@gmail.com>
2022-04-13 08:49:46 +0200

    Solve 57a84137cf1fa5f9f80000d6: Get list sum recursively

---
+++ src/Kyu7/SumRecursively.hs
@@ -0,0 +1,5 @@
+module Kyu7.SumRecursively where
+
+sumR :: [Int] -> Int
+sumR [] = 0
+sumR (x:xs) = x + sumR xs
---
+++ test/Kyu7/SumRecursivelySpec.hs
@@ -0,0 +1,13 @@
+module Kyu7.SumRecursivelySpec where
+
+import Kyu7.SumRecursively
+import Test.QuickCheck
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "The sumR function" $ do
+    it "should work for some examples" $ do
+      sumR [] `shouldBe` 0
+      sumR [1] `shouldBe` 1
+      sumR [1,1,1] `shouldBe` 3
