04e6fe3190594773ab37364498f7008b9c5a65db
tbsklg <tbs.klg@gmail.com>
2022-04-18 09:39:28 +0200

    Solve 589573e3f0902e8919000109: Simple Fun #87: Shuffled Array

---
+++ src/Kyu7/SF87.hs
@@ -0,0 +1,16 @@
+module Kyu7.SF87 where
+
+import Data.List (sort)
+
+shuffledList :: [Int] -> [Int]
+shuffledList l = findAt 0
+  where
+    findAt i
+      | last' == sumOthers = sort others
+      | otherwise = findAt (i + 1)
+      where
+        last' = last shuffle
+        sumOthers = sum . init $ shuffle
+        others = init shuffle
+        
+        shuffle = take (length l) . drop i . cycle $ l
---
+++ test/Kyu7/SF87Spec.hs
@@ -0,0 +1,25 @@
+module Kyu7.SF87Spec where
+
+import Test.Hspec
+import Kyu7.SF87
+
+import Test.HUnit (assertEqual)
+
+myTest ns exp = do
+  assertEqual msg exp actual
+  where 
+    actual = shuffledList ns
+    msg = "Failed at: " ++ showArgs
+    showArgs = show ns
+ 
+spec :: Spec
+spec = do
+  it "Fixed " $ do
+    myTest [1, 12, 3, 6, 2]   [1, 2, 3, 6]
+    myTest [1, -3, -5, 7, 2]  [-5, -3, 2, 7]
+    myTest [2, -1, 2, 2, -1]  [-1, -1, 2, 2]
+    myTest [-3, -3]           [-3]
+    myTest [-3, 0, -3]        [-3, 0]
+   
+-- the following line is optional for 8.2
+main = hspec spec
