19ee8e17751a335056c6f71681fd2eda9a13c75b
tbsklg <tbs.klg@gmail.com>
2022-04-25 21:37:07 +0200

    Solve 54381f0b6f032f933c000108: Counting power sets

---
+++ test/Kyu7/PowerSetCountingSpec.hs
@@ -0,0 +1,14 @@
+module Kyu7.PowerSetCountingSpec where
+import Kyu7.PowerSetCounting
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Simple tests" $ do
+    it "Order tests" $ do
+      powers [1,2,3] `shouldBe` 8
+      powers [2,1,3] `shouldBe` 8
+      powers [3,1,2] `shouldBe` 8
+    it "Type tests" $ do
+      powers ['1','2','3'] `shouldBe` 8
+      powers [True,False,False] `shouldBe` 8
