ce12adb6afa4fb25ba09686b641e059bfbb75242
tbsklg <tbs.klg@gmail.com>
2022-04-27 17:20:06 +0200

    Solve 53da6a7e112bd15cbc000012: Sorting Dictionaries

---
+++ src/Kyu7/Dictionaries.hs
@@ -0,0 +1,10 @@
+module Kyu7.Dictionaries where
+
+import Data.List (sortBy)
+
+sortDict :: Ord v => [(k,v)] -> [(k,v)]
+sortDict = reverse . sortBy sndFst
+
+sndFst (x, y) (x', y')
+    | y == y' = LT
+    | otherwise = compare y y' 
---
+++ test/Kyu7/DictionariesSpec.hs
@@ -0,0 +1,14 @@
+module Kyu7.DictionariesSpec where
+import Kyu7.Dictionaries (sortDict)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "sortDict" $ do
+    it "should handle dictionaries with numbers as keys" $ do
+      sortDict [(1,3),(2,2),(3,1)] `shouldBe` [(1,3),(2,2),(3,1)]
+      sortDict [(3,1),(2,2),(1,3)] `shouldBe` [(1,3),(2,2),(3,1)]
+      sortDict [(1,2),(2,4),(3,6)] `shouldBe` [(3,6),(2,4),(1,2)]
+
+    it "should handle dictionaries with strings as keys" $ do
+      sortDict [("a", 6), ("b", 2), ("c",4)] `shouldBe` [("a", 6), ("c", 4), ("b",2)]
