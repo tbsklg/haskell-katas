58743f01274beb0fd9af3ab40d28b1b8f883105c
tbsklg <tbs.klg@gmail.com>
2022-04-28 20:45:05 +0200

    Solve 54fb853b2c8785dd5e000957: Chain me

---
+++ src/Kyu7/ChainMe.hs
@@ -0,0 +1,4 @@
+module Kyu7.ChainMe (chain) where
+
+chain :: x -> [ x -> x ] -> x
+chain = foldl (\c f -> f c)
---
+++ test/Kyu7/ChainMeSpec.hs
@@ -0,0 +1,13 @@
+module Kyu7.ChainMeSpec (spec) where
+
+import Kyu7.ChainMe (chain)
+import Test.Hspec
+
+add = (+ 1)
+mul = (* 30)
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    chain 2 [add,mul] `shouldBe` 90
+    chain 42 [] `shouldBe` 42
