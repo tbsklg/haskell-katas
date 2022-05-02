b6cf7406398d272549f052a2cea7db3126f373fb
tbsklg <tbs.klg@gmail.com>
2022-05-02 15:13:07 +0200

    Solve 5550d638a99ddb113e0000a2: Josephus Permutation

---
+++ src/Kyu5/JosephusPermutation.hs
@@ -0,0 +1,20 @@
+module Kyu5.JosephusPermutation where
+
+josephus :: [a] -> Int -> [a]
+josephus [] _ = []
+josephus xs k = survivor : josephus remaining k
+    where
+        survivor = fst . dropNth k $ xs
+        remaining = snd . dropNth k $ xs
+
+dropNth :: Int -> [a] -> (a, [a])
+dropNth n xs = (xs !! (at - 1), drop at xs ++ take (at - 1) xs)
+  where
+    at
+      | n `mod` length xs == 0 = length xs
+      | otherwise = n `mod` length xs
+
+-- josephus :: [a] -> Int -> [a]
+-- josephus [] _ = []
+-- josephus ls k = let (x:xs) = drop (k - 1) $ cycle ls in
+--                 x : take (length ls - 1) xs `josephus` k
---
+++ test/Kyu5/JosephusPermutationSpec.hs
@@ -0,0 +1,15 @@
+module Kyu5.JosephusPermutationSpec where
+import Test.Hspec
+import Test.QuickCheck
+
+import Kyu5.JosephusPermutation (josephus)
+
+spec :: Spec
+spec = do
+  describe "josephus" $ do
+    it "works with integers" $ do
+      josephus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 1 `shouldBe` [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      josephus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 2 `shouldBe` [2, 4, 6, 8, 10, 3, 7, 1, 9, 5]
+
+    it "works with strings" $ do
+      josephus "CodeWars" 4 `shouldBe` "esWoCdra"
