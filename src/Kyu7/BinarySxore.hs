01d6df857cec875e6a02cb0b45cab65a11b4b9ce
tbsklg <tbs.klg@gmail.com>
2022-05-14 08:18:23 +0200

    Solve 56d3e702fc231fdf72001779: Binary sXORe

---
+++ src/Kyu7/BinarySxore.hs
@@ -0,0 +1,9 @@
+module Kyu7.BinarySxore where
+
+sxore :: Integer -> Integer
+sxore i
+    | i `mod` 4 == 0 = i
+    | i `mod` 4 == 1 = 1
+    | i `mod` 4 == 2 = i + 1
+    | i `mod` 4 == 3 = 0
+    | otherwise = i
---
+++ test/Kyu7/BinarySxoreSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.BinarySxoreSpec (spec, main) where
+import Kyu7.BinarySxore (sxore)
+import Test.Hspec
+import Test.QuickCheck
+
+tester :: Integer -> Integer -> Spec
+tester n ans = it ("should work when n = " ++ show n) $ sxore n `shouldBe` ans
+
+main = hspec spec
+spec = do
+  describe "sxore" $ do
+    tester 0 0
+    tester 1 1
+    tester 50 51
+    tester 1000000 1000000
