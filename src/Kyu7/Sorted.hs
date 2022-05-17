6f02315dda2e64af55ed2b7da5b94bb0352f8661
tbsklg <tbs.klg@gmail.com>
2022-05-17 18:37:10 +0200

    Solve 52705ed65de62b733f000064: Return a sorted list of objects

---
+++ src/Kyu7/Sorted.hs
@@ -0,0 +1,6 @@
+module Kyu7.Sorted where
+
+import Data.List (sortOn)
+
+sortList :: Ord b => (a -> b) -> [a] -> [a]
+sortList = sortOn
---
+++ test/Kyu7/SortedSpec.hs
@@ -0,0 +1,10 @@
+module Kyu7.SortedSpec where
+import Kyu7.Sorted
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "sortList" $ do
+      it "should work on some examples" $ do
+        sortList fst (zip [1..10] [20,19..]) `shouldBe` zip [1..10]  [20,19..]
+        sortList snd (zip [1..10] [20,19..]) `shouldBe` zip [10,9..1] [11..20]
