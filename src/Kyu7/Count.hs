0ed3b702926a34a7806884692e2d95d8b1685caf
tbsklg <tbs.klg@gmail.com>
2022-05-17 18:33:26 +0200

    Solve 56311e4fdd811616810000ce: Method For Counting Total Occurence Of Specific Digits

---
+++ src/Kyu7/Count.hs
@@ -0,0 +1,21 @@
+module Kyu7.Count where
+
+import Control.Arrow ((&&&))
+import Data.List (group, sort)
+
+countSpecDigits :: [Int] -> [Int] -> [(Int, Int)]
+countSpecDigits a =
+  map
+    ( \x -> case lookup x dict of
+        Just a -> (x, a)
+        _ -> (x, 0)
+    )
+  where
+    dict = map (head &&& length) . group . sort . concatMap digits $ a
+
+digits :: Integral a => a -> [a]
+digits 0 = [0]
+digits x = go x
+  where
+    go 0 = []
+    go x = go (abs x `div` 10) ++ [abs x `mod` 10]
---
+++ test/Kyu7/CountSpec.hs
@@ -0,0 +1,25 @@
+module Kyu7.CountSpec where
+
+import Test.Hspec
+import Kyu7.Count
+
+i1 = [1, 1, 2 ,3 ,1 ,2 ,3 ,4]
+d1 = [1, 3]
+i2 = [-18, -31, 81, -19, 111, -888]
+d2 = [1, 8, 4]
+i3 = [-77, -65, 56, -79, 6666, 222]
+d3 = []
+i4 = []
+d4 = [1, 8, 4]
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  it "handles fixed cases" $ do
+    countSpecDigits i1 d1 `shouldBe` [(1, 3), (3, 2)]
+    countSpecDigits i2 d2 `shouldBe` [(1, 7), (8, 5), (4, 0)]
+    countSpecDigits i3 d3 `shouldBe` []
+    countSpecDigits i4 d4 `shouldBe` [(1, 0), (8, 0), (4, 0)]
+      
+-- the following line is optional for 8.2
+main = hspec spec
