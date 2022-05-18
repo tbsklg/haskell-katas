d7159a08faf626625dfd8305b07f7f9e74539191
tbsklg <tbs.klg@gmail.com>
2022-05-18 23:30:58 +0200

    Solve 55b1fd84a24ad00b32000075: Selective fear of numbers

---
+++ src/Kyu7/Fear.hs
@@ -0,0 +1,11 @@
+module Kyu7.Fear {- of the dark -} where
+
+amIAfraid :: String -> Int -> Bool
+amIAfraid "Monday"    12 = True
+amIAfraid "Tuesday"    n = n > 95
+amIAfraid "Wednesday" 34 = True
+amIAfraid "Thursday"   0 = True
+amIAfraid "Friday"     n = even n
+amIAfraid "Saturday"  56 = True
+amIAfraid "Sunday"     n = abs n == 666
+amIAfraid _            _ = False
