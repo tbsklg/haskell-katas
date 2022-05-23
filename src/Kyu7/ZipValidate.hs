6b1319acd4bc07a5aba3918dc326f7d2e8003630
tbsklg <tbs.klg@gmail.com>
2022-05-23 08:31:11 +0200

    Solve 552e45cc30b0dbd01100001a: Russian postal code checker

---
+++ src/Kyu7/ZipValidate.hs
@@ -0,0 +1,12 @@
+module Kyu7.ZipValidate where
+
+import Data.Char (isDigit)
+
+zipValidate :: String -> Bool
+zipValidate "" = False
+zipValidate p@(x:xs)
+    | not . (==) 6 . length $ p = False
+    | x == '0' || x == '5' || x == '7' || x == '8' || x == '9' = False
+    | otherwise = foldl (\y x -> y && isDigit x) True p
+
+--   | any (not . isDigit) xs = False
