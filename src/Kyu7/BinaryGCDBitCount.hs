9f9133589ef09bc4498c14c6aee4d93308e095b5
tbsklg <tbs.klg@gmail.com>
2022-05-30 08:51:35 +0200

    Solve 54b45c37041df0caf800020f: Greatest Common Divisor Bitcount

---
+++ src/Kyu7/BinaryGCDBitCount.hs
@@ -0,0 +1,10 @@
+module Kyu7.BinaryGCDBitCount where
+
+binaryGCD :: Int -> Int -> Int
+binaryGCD x = sum . bin . gcd x
+
+bin :: Integral a => a -> [a]
+bin 0 = []
+bin x = bin (x `div` 2) ++ [x `rem` 2]
+
+-- binaryGCD n = popCount . gcd n
---
+++ test/Kyu7/BinaryGCDBitCountSpec.hs
@@ -0,0 +1,25 @@
+module Kyu7.BinaryGCDBitCountSpec where
+
+import Test.Hspec
+import Kyu7.BinaryGCDBitCount
+
+
+import Test.HUnit (assertEqual)
+
+myTest m n  exp = do
+  print $ "Looking for: " ++ show exp
+  assertEqual msg exp actual
+  where 
+    actual = binaryGCD m n 
+    msg = "Failed at: " ++ showArgs
+    showArgs = show m ++ show n
+    
+spec :: Spec
+spec = do
+  it "Fixed cases" $ do
+    myTest 300    45       4
+    myTest 0      0        0
+    myTest 100000 10000000 6
+
+-- the following line is optional for 8.2
+main = hspec spec
