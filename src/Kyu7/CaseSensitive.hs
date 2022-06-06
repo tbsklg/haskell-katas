3efb60558bd16482ef3bcd98229a46e977a43a73
tbsklg <tbs.klg@gmail.com>
2022-06-06 11:11:11 +0200

    Solve 5a805631ba1bb55b0c0000b8: Case-sensitive!

---
+++ src/Kyu7/CaseSensitive.hs
@@ -0,0 +1,10 @@
+module Kyu7.CaseSensitive where
+
+import Data.Char (isLower, isUpper)
+
+caseSensitive :: String -> (Bool, String)
+caseSensitive xs = (l, r)
+  where
+    l = all isLower xs
+    r = filter isUpper xs
+    
---
+++ test/Kyu7/CaseSensitiveSpec.hs
@@ -0,0 +1,13 @@
+module Kyu7.CaseSensitiveSpec where
+
+import Kyu7.CaseSensitive
+import Test.Hspec
+
+spec = do
+  describe "Case Sensitive" $ do
+    it "Basic tests" $ do
+      caseSensitive "asd" `shouldBe` (True,"")
+      caseSensitive "cellS" `shouldBe` (False,"S")    
+      caseSensitive "z" `shouldBe` (True,"")
+      caseSensitive "" `shouldBe` (True,"")  
+      caseSensitive "codeWarS" `shouldBe` (False,"WS")
