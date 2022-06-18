f32d20b764de68d2ebf002baf57bcf101dfbea70
tbsklg <tbs.klg@gmail.com>
2022-06-18 10:11:12 +0200

    Solve 588453ea56daa4af920000ca: Simple Fun #9: Array Packing

---
+++ src/Kyu7/Packing.hs
@@ -0,0 +1,14 @@
+module Kyu7.Packing where
+
+arrayPacking :: [Integer] -> Integer
+arrayPacking = bintodec . concatMap (eightBit . binary) . reverse
+
+bintodec :: [Integer] -> Integer
+bintodec = foldr (\(i,x) y -> y + x * 2 ^ i) 0 . zip [0,1 ..] . reverse
+
+eightBit :: Num a => [a] -> [a]
+eightBit xs = replicate (8 - length xs) 0 ++ xs
+
+binary :: Integral a => a -> [a]
+binary 0 = []
+binary x = binary (x `div` 2) ++ [x `mod` 2]
---
+++ test/Kyu7/PackingSpec.hs
@@ -0,0 +1,16 @@
+module Kyu7.PackingSpec where
+
+import Test.Hspec
+import Kyu7.Packing
+
+spec :: Spec
+spec = do
+  it "a few fixed cases " $ do
+    arrayPacking [24, 85, 0] `shouldBe` 21784
+    arrayPacking [1, 1] `shouldBe` 257
+    arrayPacking [0] `shouldBe` 0
+    arrayPacking [255, 255, 255, 255] `shouldBe` 4294967295
+    arrayPacking [23, 45, 39] `shouldBe` 2567447
+
+-- the following line is optional for 8.2
+main = hspec spec
