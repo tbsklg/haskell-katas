0b40cce9eadf57a0ba4e30518648cb79a25a83c9
tbsklg <tbs.klg@gmail.com>
2022-06-20 16:41:47 +0200

    Solve 5a34f087c5e28462d9000082: Swap the head and the tail

---
+++ src/Kyu7/SwapHeadTail.hs
@@ -0,0 +1,8 @@
+module Kyu7.SwapHeadTail where
+
+swapHeadAndTail :: [Int] -> [Int]
+swapHeadAndTail xs
+    | odd . length $ xs = drop (middle + 1) xs ++ [xs !! middle] ++ take middle xs
+    | otherwise = drop middle xs ++ take middle xs
+    where 
+        middle = length xs `div` 2
---
+++ test/Kyu7/SwapHeadTailSpec.hs
@@ -0,0 +1,11 @@
+module Kyu7.SwapHeadTailSpec where
+import Kyu7.SwapHeadTail
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Swap head and tail" $ do
+    it "Basic tests" $ do
+      swapHeadAndTail [ 1, 2, 3, 4, 5 ] `shouldBe` [ 4, 5, 3, 1, 2 ]
+      swapHeadAndTail [ -1, 2] `shouldBe` [2, -1 ]
+      swapHeadAndTail [ 1, 2, -3, 4, 5, 6, -7, 8 ] `shouldBe` [ 5, 6, -7, 8, 1, 2, -3, 4 ]
