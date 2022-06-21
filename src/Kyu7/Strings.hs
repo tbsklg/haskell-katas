8dca22bdfea688293ef448a8e4a1827ff9b54705
tbsklg <tbs.klg@gmail.com>
2022-06-21 09:48:04 +0200

    Solve 56a24b4d9f3671584d000039: Are there doubles?

---
+++ src/Kyu7/Strings.hs
@@ -0,0 +1,8 @@
+module Kyu7.Strings where
+
+import Data.Char (toLower)
+
+doubleCheck :: [Char] -> Bool
+doubleCheck str = foldl (\y (c, n) -> y || c == n) False . zip (tail lst) $ lst
+    where
+        lst = map toLower str
---
+++ test/Kyu7/StringsSpec.hs
@@ -0,0 +1,17 @@
+module Kyu7.StringsSpec (spec, main) where
+
+import Kyu7.Strings (doubleCheck)
+import Test.Hspec
+
+main = hspec spec
+spec = do
+  describe "doubleCheck" $ do
+    it "works for some examples" $ do
+      doubleCheck "abca" `shouldBe` False
+      doubleCheck "aabc" `shouldBe` True
+      doubleCheck "a 11 c d" `shouldBe` True
+      doubleCheck "AabBcC" `shouldBe` True
+      doubleCheck "a b  c" `shouldBe` True
+      doubleCheck "a b c d e f g h i h k" `shouldBe` False
+      doubleCheck "2020" `shouldBe` False
+      doubleCheck "a!@€£#$%^&*()_-+=}]{[|\':;?/>.<,~" `shouldBe` False
