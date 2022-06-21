7ef621ddc9a70a21d52f349b3800a07001e7ed02
tbsklg <tbs.klg@gmail.com>
2022-06-21 13:37:52 +0200

    Solve 5bce125d3bb2adff0d000245: London CityHacker

---
+++ src/Kyu7/LondonCityHacker.hs
@@ -0,0 +1,17 @@
+module Kyu7.LondonCityHacker (londonCityHacker, Journey (..)) where
+
+import Text.Printf (printf)
+
+data Journey = Bus Int | Tube String deriving (Show, Eq)
+
+londonCityHacker :: [Journey] -> String
+londonCityHacker = print' . costs
+
+costs :: [Journey] -> Double
+costs [] = 0
+costs (Bus _: Bus _: xs) = 1.50 + costs xs
+costs (Tube _ : xs) = 2.40 + costs xs
+costs (Bus _: xs) = 1.50 + costs xs
+
+print' :: Double -> String
+print' = printf "£%.2f"
---
+++ test/Kyu7/LondonCityHackerSpec.hs
@@ -0,0 +1,15 @@
+module Kyu7.LondonCityHackerSpec (spec) where
+
+import Kyu7.LondonCityHacker (Journey(..), londonCityHacker)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    londonCityHacker [Tube "Northern", Tube "Central", Bus 243, Bus 1, Tube "Victoria"] `shouldBe` "£8.70"
+    londonCityHacker [Bus 12, Tube "Central", Tube "Circle", Bus 21] `shouldBe` "£7.80"
+    londonCityHacker [Tube "Piccidilly", Bus 56] `shouldBe` "£3.90"
+    londonCityHacker [Tube "Northern", Tube "Central", Tube "Circle"] `shouldBe` "£7.20"
+    londonCityHacker [Tube "Piccidilly", Bus 56, Bus 93, Bus 243] `shouldBe` "£5.40"
+    londonCityHacker [Bus 386, Bus 56, Bus 1, Bus 876] `shouldBe` "£3.00"
+    londonCityHacker [] `shouldBe` "£0.00"
