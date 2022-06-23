f450d0ce1ce91be6bfffeeb25499189011acf662
tbsklg <tbs.klg@gmail.com>
2022-06-23 20:35:39 +0200

    Solve 5507309481b8bd3b7e001638: Simple Maths Test

---
+++ src/Kyu7/NumberProperties.hs
@@ -0,0 +1,10 @@
+module Kyu7.NumberProperties where
+
+data Property = Property Bool   Bool  Bool deriving (Show, Eq)
+--                       prime  even  10*
+
+numberProperty :: Integral n => n -> Property
+numberProperty n = Property (isPrime n) (even n) (n `mod` 10 == 0)
+
+isPrime :: Integral a => a -> Bool
+isPrime i = i > 1 && all (\x -> i `mod` x > 0) [2 .. (i - 1)]
---
+++ test/Kyu7/NumberPropertiesSpec.hs
@@ -0,0 +1,20 @@
+module Kyu7.NumberPropertiesSpec where
+    
+import Kyu7.NumberProperties (numberProperty, Property(..))
+import Test.Hspec 
+import Test.QuickCheck (forAll, arbitrary, Gen, property)
+
+spec :: Spec
+spec = do
+  describe "numberProperty" $ do
+    it "should work for some examples" $ do
+      numberProperty 1  `shouldBe` Property False False False
+      numberProperty 2  `shouldBe` Property True  True  False
+      numberProperty 3  `shouldBe` Property True  False False
+      numberProperty 7  `shouldBe` Property True  False False
+      numberProperty 10 `shouldBe` Property False True  True      
+      numberProperty (-7)  `shouldBe` Property False False False
+      numberProperty (-10) `shouldBe` Property False True  True
+    it "should work for multiples of 10" $ do
+      property $ forAll (fmap (10*) arbitrary :: Gen Integer) $ \n ->
+        numberProperty n `shouldBe` Property False True True
