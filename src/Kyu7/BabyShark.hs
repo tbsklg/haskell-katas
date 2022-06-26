cf44b4e0ff3a229a2e84b06927a1e95ab92cd745
tbsklg <tbs.klg@gmail.com>
2022-06-26 10:10:10 +0200

    Solve 5d076515e102162ac0dc514e: Baby shark lyrics generator

---
+++ src/Kyu7/BabyShark.hs
@@ -0,0 +1,12 @@
+module Kyu7.BabyShark (babySharkLyrics) where
+
+import Data.List
+
+babySharkLyrics=unlines.(++)(concatMap c["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"])$["Run away,…"]
+
+a=unwords.replicate 6$"doo"
+
+b x=x++", "++a
+s s=s++" shark"
+
+c x=replicate 3 (b x)++[x ++ "!"]
