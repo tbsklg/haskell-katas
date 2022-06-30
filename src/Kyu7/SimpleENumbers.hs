70c6a17acf1b408c1aa275e547c7bc58fb2e2da6
tbsklg <tbs.klg@gmail.com>
2022-06-30 08:59:12 +0200

    Solve 5b93f268563417c7ed0001bd: Simple eviternity numbers

---
+++ src/Kyu7/SimpleENumbers.hs
@@ -0,0 +1,18 @@
+module Kyu7.SimpleENumbers where 
+
+solve :: Int -> Int -> Int 
+solve a b = length . filter eviternity $ [a .. b]
+
+eviternity :: Integral a => a -> Bool
+eviternity 0 = False
+eviternity x = valid && eight >= five && five >= three
+    where
+        valid = all (\x -> x `elem` [8,5,3]) . digits $ x
+
+        eight = length . filter (==8) . digits $ x
+        five = length . filter (==5) . digits $ x
+        three = length . filter (==3) . digits $ x
+
+digits :: Integral a => a -> [a]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `mod` 10]
---
+++ test/Kyu7/SimpleENumbersSpec.hs
@@ -0,0 +1,12 @@
+module Kyu7.SimpleENumbersSpec where
+import Kyu7.SimpleENumbers
+import Test.Hspec
+
+spec = do
+  describe "Simple eviternity numbers" $ do
+    it "Basic tests" $ do
+      solve 0 100 `shouldBe` 4
+      solve 0 1000 `shouldBe` 14
+      solve 0 10000 `shouldBe` 37
+      solve 0 100000 `shouldBe` 103
+      solve 0 500000 `shouldBe` 148
