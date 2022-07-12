0efe3ed5079c78da7adf2a935f35412c2457a3af
tbsklg <tbs.klg@gmail.com>
2022-07-12 12:18:19 +0200

    Solve 5270d0d18625160ada0000e4: Greed is Good

---
+++ src/Kyu5/Score.hs
@@ -0,0 +1,13 @@
+module Kyu5.Score where
+
+score :: [Int] -> Int
+score [] = 0
+score l@(x : xs) = score' . length . filter (== x) $ l
+  where
+    score' 3
+      | x == 1 = 1000 + score (filter (/=x) xs)
+      | otherwise = x * 100 + score (filter (/=x) xs)
+    score' _
+      | x == 1 = 100 + score xs
+      | x == 5 = 50 + score xs
+      | otherwise = score xs
---
+++ test/Kyu5/ScoreSpec.hs
@@ -0,0 +1,16 @@
+module Kyu5.ScoreSpec where
+
+import Kyu5.Score
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = do
+  it "should value this as worthless" $ do
+    score [2, 3, 4, 6, 2] `shouldBe` 0
+  
+  it "should value this triplet correctly" $ do
+    score [4, 4, 4, 3, 3] `shouldBe` 400
+
+  it "should value this mixed set correctly" $ do
+    score [2, 4, 4, 5, 4] `shouldBe` 450
+    score [1, 1, 1, 3, 1] `shouldBe` 1100
