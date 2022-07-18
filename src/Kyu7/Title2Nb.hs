9334462807607ca95458f0c68ee74b94227503d6
tbsklg <tbs.klg@gmail.com>
2022-07-18 15:07:40 +0200

    Solve 55ee3ebff71e82a30000006a: Excel sheet column numbers

---
+++ src/Kyu7/Title2Nb.hs
@@ -0,0 +1,13 @@
+module Kyu7.Title2Nb where
+
+titleToNb :: String -> Integer
+titleToNb xs =
+  foldl (\y (i,x) -> y + x * 26 ^ i) (pos . last $ xs)
+    . zip [1 ..]
+    . map pos
+    . reverse
+    . init
+    $ xs
+
+pos :: Char -> Integer
+pos x = (fromIntegral . fromEnum $ x) - 64
