e80f4caca3204bfd4773708e3cbacc1b16ae4944
tbsklg <tbs.klg@gmail.com>
2022-07-19 10:26:13 +0200

    Solve 5a1ebc2480171f29cf0000e5: Sort rectangles and circles by area II

---
+++ src/Kyu7/SortByArea.hs
@@ -0,0 +1,21 @@
+module Kyu7.SortByArea (sortByArea, Shape(..)) where
+
+import Data.List (sortBy)
+
+data Shape = Rectangle { width, length :: Double } | Circle { radius :: Double } deriving (Show, Eq)
+
+sortByArea :: [Shape] -> [Shape]
+sortByArea [] = []
+sortByArea (x:xs) = sortBy area (x:xs)
+
+area :: Shape -> Shape -> Ordering
+area Rectangle {Kyu7.SortByArea.length = l, width = w} Circle {radius = r} = compare (l * w) (r ^ 2 * pi)
+area Rectangle {Kyu7.SortByArea.length = l, width = w} Rectangle {Kyu7.SortByArea.length = l', width = w'} = compare (l * w) (l' * w')
+area Circle {radius = r} Rectangle {Kyu7.SortByArea.length = l, width = w} = compare (r ^ 2 * pi) (l * w) 
+area Circle {radius = r} Circle {radius = r'} = compare (r ^ 2 * pi) (r' ^ 2 * pi)
+
+-- area (Rectangle w h) = w * h
+-- area (Circle r) = pi * r**2
+
+-- sortByArea :: [Shape] -> [Shape]
+-- sortByArea = sortOn area
---
+++ test/Kyu7/SortByAreaSpec.hs
@@ -0,0 +1,12 @@
+module Kyu7.SortByAreaSpec (spec) where
+
+import Kyu7.SortByArea (sortByArea, Shape(..))
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    sortByArea [ Rectangle 4.23 6.43, Circle 1.23, Circle 3.444, Rectangle 1.342 3.212 ]
+      `shouldBe` [ Rectangle 1.342 3.212, Circle 1.23, Rectangle 4.23 6.43, Circle 3.444 ]
+    sortByArea [ Rectangle 2 5, Circle 6 ] `shouldBe` [ Rectangle 2 5, Circle 6 ]
+    sortByArea [] `shouldBe` []
