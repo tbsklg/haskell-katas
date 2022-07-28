bf1adde8f7535e2841b2811ada565607a375c10f
tbsklg <tbs.klg@gmail.com>
2022-07-28 13:30:23 +0200

    Update 5751aa92f2dac7695d000fb0: Mythical Heads and Tails

--- src/Kyu7/Beats.hs
+++ src/Kyu7/Beats.hs
@@ -10,3 +10,8 @@ beasts heads tails
     hydras = tails - orthus
     validTails = orthus + hydras == tails
     validHeads = orthus * 2 + hydras * 5 == heads
+
+-- beasts :: Integer -> Integer -> Maybe (Integer, Integer)
+-- beasts heads tails = if null b then Nothing else head b
+--   where
+--     b = [Just (x,y) | x <- [0..tails], y <- [0..tails], x*2+y*5 == heads, x+y == tails]
