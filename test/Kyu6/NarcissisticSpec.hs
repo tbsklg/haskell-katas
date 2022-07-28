f1754be5eb5c65e1417ee775866564975e375b5d
tbsklg <tbs.klg@gmail.com>
2022-07-28 16:51:22 +0200

    Solve 5287e858c6b5a9678200083c: Does my number look big in this?

---
+++ test/Kyu6/NarcissisticSpec.hs
@@ -0,0 +1,14 @@
+module Kyu6.NarcissisticSpec where
+
+import Kyu6.Narcissistic (narcissistic)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "narcissistic" $ do
+    it "should work for some examples" $ do
+      narcissistic 7    `shouldBe` True
+      narcissistic 12   `shouldBe` False
+      narcissistic 370  `shouldBe` True
+      narcissistic 371  `shouldBe` True
+      narcissistic 1634 `shouldBe` True
