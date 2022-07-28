2b10ee21ff788ddd52bc13432bd5a33a0ff9ab11
tbsklg <tbs.klg@gmail.com>
2022-07-28 07:52:19 +0200

    Solve 56f5594a575d7d3c0e000ea0: Conference Traveller

---
+++ src/Kyu7/Conference.hs
@@ -0,0 +1,8 @@
+module Kyu7.Conference where
+
+conferencePicker :: [String] -> [String] -> Maybe String
+conferencePicker citiesVisited citiesOffered = case dropWhile(`elem` citiesVisited) citiesOffered of
+    [] -> Nothing
+    cities -> Just . head $ cities
+
+-- listToMaybe
---
+++ test/Kyu7/ConferenceSpec.hs
@@ -0,0 +1,18 @@
+module Kyu7.ConferenceSpec where
+
+import Kyu7.Conference (conferencePicker)
+
+import Test.Hspec
+import Test.QuickCheck
+
+data Cities = Cities
+
+spec :: Spec
+spec = do
+  describe "conferencePicker" $ do
+    context "static tests" $ do
+      it "works if there is one unvisited city" $ do
+        conferencePicker ["Mexico City", "Johannesburg", "Stockholm", "Osaka", "Saint Petersburg", "London"] ["Stockholm", "Paris", "Melbourne"] `shouldBe` Just "Paris"
+
+      it "returns Nothing if all cities have been visited" $ do
+        conferencePicker ["Berlin", "Paris"] ["Paris", "Berlin"] `shouldBe` Nothing
