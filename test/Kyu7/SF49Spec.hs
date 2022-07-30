123db8d5621c5d4b451d70ee01e689ff5bb4cab1
tbsklg <tbs.klg@gmail.com>
2022-07-30 08:25:09 +0200

    Solve 5888514674b58e929a000036: Simple Fun #49: Decipher

---
+++ test/Kyu7/SF49Spec.hs
@@ -0,0 +1,15 @@
+module Kyu7.SF49Spec where
+
+import Test.Hspec
+import Kyu7.SF49
+
+-- `spec` of type `Spec` must exist
+spec :: Spec
+spec = do
+  it "works on fixed cases" $ do
+    decipher "98" `shouldBe` "b"
+    decipher "122" `shouldBe` "z"
+    decipher "10197115121" `shouldBe` "easy"
+
+-- the following line is optional for 8.2
+main = hspec spec
