5b37b243d4344d2da3e81091fad9eb4ca94d85f4
tbsklg <tbs.klg@gmail.com>
2022-08-04 08:54:16 +0200

    Solve 57d27a0a26427672b900046f: Duck Shoot - Easy Version

---
+++ src/Kyu7/DuckShoot.hs
@@ -0,0 +1,12 @@
+module Kyu7.DuckShoot where
+
+duckShoot :: Int -> Float-> String -> String 
+duckShoot ammo aim ducks = go ducks strikes 
+    where
+        go [] _ = []
+        go xs 0 = xs
+        go (x:xs) s
+            | x == '2' = 'X' : go xs (s - 1)
+            | otherwise = x : go xs s
+
+        strikes = floor (fromIntegral ammo * aim)
