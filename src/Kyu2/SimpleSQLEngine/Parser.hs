a3ba3b85280a4b2e714248dd478e2d4b2c700ed9
tbsklg <tbs.klg@gmail.com>
2022-08-11 07:43:52 +0200

    --amend

--- src/Kyu2/SimpleSQLEngine/Parser.hs
+++ src/Kyu2/SimpleSQLEngine/Parser.hs
@@ -2,6 +2,13 @@ module Kyu2.SimpleSQLEngine.Parser () where
 
 import Kyu2.SimpleSQLEngine.Tokenizer (Token (..))
 
+data AST = AST {ast_type :: string, variant :: string, statement :: [Statement]} deriving (Show, Eq)
 
-parse :: [Token] -> String
-parse = 
+data Statement = SelectStatement | FromStatement
+
+data SelectStatement = SelectStatement {ast_type :: string, variant :: string}
+
+data FromStatement = FromStatement {ast_type :: string, variant :: string}
+
+parse :: [Token] -> AST
+parse = undefined
