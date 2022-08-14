37e552208ce93936fd07f7604bda9ed7cf8701ad
tbsklg <tbs.klg@gmail.com>
2022-08-14 10:10:10 +0200

    Solve 5b190aa7803388ec97000054: Tram Capacity

---
+++ src/Kyu7/TramCapacity.hs
@@ -0,0 +1,8 @@
+module Kyu7.TramCapacity where
+
+tram :: Int -> [Int] -> [Int] -> Int
+tram stops descending = maximum
+    . take stops
+    . tail
+    . scanl (\y (d,o) -> y + (o - d)) 0
+    . zip descending
