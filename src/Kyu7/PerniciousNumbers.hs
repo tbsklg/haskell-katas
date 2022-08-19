3a84852e9fce412160e65c696bdca4958f386dae
tbsklg <tbs.klg@gmail.com>
2022-08-19 10:10:10 +0200

    Solve 56e195d02bb22479e50016af: Pernicious Numbers

---
+++ src/Kyu7/PerniciousNumbers.hs
@@ -0,0 +1,31 @@
+module Kyu7.PerniciousNumbers where
+
+import Data.Bits (popCount )
+
+class PerniciousNumbers a where
+  pernicious :: a -> Either String [Int]
+
+instance PerniciousNumbers Int where
+  pernicious n =  intPernicious n
+
+instance PerniciousNumbers Float where
+  pernicious f = floatPernicious f
+
+intPernicious :: Int -> Either String [Int]
+intPernicious i
+    | null primes = Left "No pernicious numbers"
+    | otherwise = Right primes
+    where
+        primes = filter (isPrime . popCount) [0 .. i]
+
+floatPernicious :: Float -> Either String [Int]
+floatPernicious i
+    | null primes = Left "No pernicious numbers"
+    | otherwise = Right primes
+    where
+        primes = filter (isPrime . popCount) [0 .. floor i]
+
+isPrime :: Integral a => a -> Bool
+isPrime 0 = False
+isPrime 1 = False
+isPrime i = all (\x -> i `mod` x > 0) [2 .. (i - 1)]
