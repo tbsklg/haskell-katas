27ad167b6e7f8de29b8a6cba5bfad4938645ee1c
tbsklg <tbs.klg@gmail.com>
2022-08-21 10:10:10 +0200

    Solve 5a66ea69e6be38219f000110: Single character palindromes II

---
+++ src/Kyu7/SingleCharPalins.hs
@@ -0,0 +1,10 @@
+module Kyu7.SingleCharPalins where
+
+solve :: String -> Bool
+solve xs
+  | isPalindrom xs && (even . length $ xs) = False
+  | isPalindrom xs && (odd . length $ xs) = True
+  | otherwise = (==) 2 . length . filter (uncurry (/=)) . zip xs $ reverse xs
+
+isPalindrom :: Eq a => [a] -> Bool
+isPalindrom xs = xs == reverse xs
---
+++ test/Kyu7/SingleCharPalinsSpec.hs
@@ -0,0 +1,14 @@
+module Kyu7.SingleCharPalinsSpec where
+import Kyu7.SingleCharPalins
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Single Character Palindromes II" $ do
+    it "Basic tests" $ do
+      solve "abba" `shouldBe` False
+      solve "abbaa" `shouldBe` True
+      solve "abbx" `shouldBe` True
+      solve "aa" `shouldBe` False
+      solve "ab" `shouldBe` True
+      solve "abcba" `shouldBe` True
