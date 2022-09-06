5247c91dd0f636c0b0911e5f2f25e66d959e1ccb
tbsklg <tbs.klg@gmail.com>
2022-09-06 14:35:49 +0200

    Solve 568ade64cfd7a55d9300003e: Tube strike options calculator

---
+++ src/Kyu7/Tube.hs
@@ -0,0 +1,29 @@
+module Kyu7.Tube where
+import Debug.Trace (traceShow)
+
+-- import Codewars.Kata.Tube.Types
+
+data Decision = Bus | Walk deriving (Eq, Show)
+
+calculator ::
+  -- | the distance you would have to walk
+  Double ->
+  -- | the distance the bus would travel
+  Double ->
+  -- | the distance you have to *walk* to the bus
+  Double ->
+  -- | your decision whether to take the Bus or Walk
+  Decision
+calculator distance busDrive busWalk
+  | walk > 2 = Bus
+  | walk < (10 / 60) || walk <= bus = Walk
+  | otherwise = Bus
+  where
+    walk = distance / walkPerHour
+    bus = busDrive / busPerHour + busWalk / walkPerHour
+
+walkPerHour :: Double
+walkPerHour = 5
+
+busPerHour :: Double
+busPerHour = 8
