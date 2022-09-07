76d3008f8da0bd1b04557cf91a7338f074e705bd
tbsklg <tbs.klg@gmail.com>
2022-09-07 10:10:10 +0200

    Solve 5822b65bb81f702016000026: Unix command line `ls -l` extract the file type.

---
+++ src/Kyu7/Attribute.hs
@@ -0,0 +1,14 @@
+module Kyu7.Attribute where
+
+data FileType = File | Directory | Symlink | Character | Block | Door | Socket | Pipe deriving (Show, Eq)
+
+linuxType :: String -> FileType
+linuxType ('-':xs) = File
+linuxType ('l':xs) = Symlink
+linuxType ('c':xs) = Character
+linuxType ('b':xs) = Block
+linuxType ('D':xs) = Door
+linuxType ('d':xs) = Directory
+linuxType ('s':xs) = Socket
+linuxType ('p':xs) = Pipe
+linuxType _ = error "Unknown FileType"
---
+++ test/Kyu7/AttributeSpec.hs
@@ -0,0 +1,25 @@
+module Kyu7.AttributeSpec where
+
+import Kyu7.Attribute (linuxType)
+import Kyu7.Attribute (linuxType, FileType(..))
+import Control.Monad (forM, forM_)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = describe "linuxType" $ do
+  let types = [File, Directory, Symlink, Character, Block, Door, Socket, Pipe]
+  let short = "-dlcbDsp"
+  -- let's check all combinations
+  forM_ (zip short types) $ \(c,f) ->
+    it ("returns " ++ show f ++ " for files starting with " ++ ['"',c,'"']) $
+      -- create a complete permission string
+      forAll (randomPerm c) $ \ls ->
+        linuxType ls `shouldBe` f
+
+-- | Creates a random permission, starting with the given
+--   character
+randomPerm :: Char -> Gen String
+randomPerm c = fmap (c:) $ forM "rwxrwxrwx" $ \x -> do  
+      b <- arbitrary
+      return $ if b then x else '-'
