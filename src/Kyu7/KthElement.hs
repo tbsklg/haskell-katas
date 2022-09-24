4d9e847b15928529f6d8bc877831dfd23874c7c6
tbsklg <tbs.klg@gmail.com>
2022-09-24 12:54:21 +0200

    Solve 54411212cf36259c04000f04: Find the k-th Element of a list safely

---
+++ src/Kyu7/KthElement.hs
@@ -0,0 +1,11 @@
+module Kyu7.KthElement where
+
+import Prelude hiding ((!!))
+
+-- | Returns Just the k-th element of the list, or Nothing if k is out of bounds.
+elementAt :: Int -> [a] -> Maybe a
+elementAt n xs
+    | n <= 0 = Nothing
+    | otherwise = case drop (n - 1) xs of
+        [] -> Nothing
+        a -> Just . head $ a
---
+++ test/Kyu7/KthElementSpec.hs
@@ -0,0 +1,29 @@
+module Kyu7.KthElementSpec where
+import Kyu7.KthElement (elementAt)
+
+import Test.Hspec
+import Test.QuickCheck
+import Codewars
+
+spec :: Spec
+spec = do
+  describe "elementAt k [1..10]" $ do
+    it "should be Just 1"  $ elementAt 1    [1..10] `shouldBe` Just 1
+    it "should be Just 4"  $ elementAt 4    [1..10] `shouldBe` Just 4
+    it "should be Just 10" $ elementAt 10   [1..10] `shouldBe` Just 10
+    it "should be Nothing" $ elementAt (-1) [1..10] `shouldBe` Nothing
+    it "should be Nothing" $ elementAt 0    [1..10] `shouldBe` Nothing
+    it "should be Nothing" $ elementAt 11   [1..10] `shouldBe` Nothing    
+  describe "[1..]" $ do
+    it "elementAt k [1..] should be Just k provided k >= 1" $ do
+      property $ forAll (suchThat arbitrary (>= 1)) $
+        \k -> elementAt k [1..] `shouldBe` Just k     
+    it "elementAt (k+1) [1..k] should be Nothing provided k >= 1" $ do
+      property $ forAll (suchThat arbitrary (>= 1)) $
+        \k -> elementAt (k+1) [1..k] `shouldBe` Nothing
+    it "elementAt k [1..] should be Nothing provided k < 1" $ do
+      property $ forAll (suchThat arbitrary (< 1)) $
+        \k -> elementAt k [1..] `shouldBe` Nothing
+  describe "Can't use builtins..." $ do
+    it "Prelude.!! and Data.List are disabled" $ do
+      solutionShouldHideAll [FromModule "Prelude" "!!", Module "Data.List"]
