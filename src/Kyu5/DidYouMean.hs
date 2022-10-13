daafe19b967a783f3582fe03576f44d29836886f
Tobias Klug <tbs.klg@gmail.com>
2022-10-13 10:10:10 +0200

    Solve 5259510fc76e59579e0009d4: Did you mean ...?

---
+++ src/Kyu5/DidYouMean.hs
@@ -0,0 +1,29 @@
+module Kyu5.DidYouMean (findMostSimilar) where
+
+import Data.Array (array, listArray, (!))
+import Data.Foldable (minimumBy)
+import Data.Function (on)
+
+-- see https://en.wikipedia.org/wiki/Levenshtein_distance
+-- see https://www.reddit.com/r/programming/comments/w4gs6/levenshtein_distance_in_haskell/
+
+findMostSimilar :: [String] -> String -> String
+findMostSimilar dictionary term = fst . minimumBy (compare `on` snd) . map (\x -> (x, levenshtein x term)) $ dictionary
+  where
+    levenshtein xs ys = levMemo ! (n, m)
+      where
+        levMemo = array ((0, 0), (n, m)) [((i, j), lev i j) | i <- [0 .. n], j <- [0 .. m]]
+        n = length xs
+        m = length ys
+        xa = listArray (1, n) xs
+        ya = listArray (1, m) ys
+        lev 0 v = v
+        lev u 0 = u
+        lev u v
+          | xa ! u == ya ! v = levMemo ! (u -1, v -1)
+          | otherwise =
+            1 + minimum
+                [ levMemo ! (u, v -1),
+                  levMemo ! (u -1, v),
+                  levMemo ! (u -1, v -1)
+                ]
---
+++ test/Kyu5/DidYouMeanSpec.hs
@@ -0,0 +1,19 @@
+module Kyu5.DidYouMeanSpec (spec) where
+
+import Kyu5.DidYouMean (findMostSimilar)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+
+    let fruits = findMostSimilar ["cherry", "pineapple", "melon", "strawberry", "raspberry"]
+    fruits "strawbery" `shouldBe` "strawberry"
+    fruits "berry" `shouldBe` "cherry"
+
+    let things = findMostSimilar ["stars", "mars", "wars", "codec", "codewars"]
+    things "coddwars" `shouldBe` "codewars"
+
+    let languages = findMostSimilar ["coffeescript", "haskell", "java", "javascript", "php", "python", "ruby"]
+    languages "heaven" `shouldBe` "java"
+    languages "javascript" `shouldBe` "javascript"
