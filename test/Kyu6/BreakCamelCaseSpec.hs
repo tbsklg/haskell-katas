5624febd2501a3c342f46f48c96324f71f758f76
Tobias Klug <tbs.klg@gmail.com>
2022-10-15 11:26:33 +0200

    Solve 5208f99aee097e6552000148: Break camelCase

---
+++ test/Kyu6/BreakCamelCaseSpec.hs
@@ -0,0 +1,19 @@
+module Kyu6.BreakCamelCaseSpec (spec, main) where
+import Kyu6.BreakCamelCase (solution)
+
+import Test.Hspec
+import Test.QuickCheck
+import Data.Char (toUpper)
+
+main = hspec spec
+spec =
+  describe "break camelCase" $ do
+    it "should work for some small examples" $ do
+      solution "HelloWorld"     `shouldBe` "Hello World"
+      solution "helloWorld"     `shouldBe` "hello World"
+      solution "camelCase"      `shouldBe` "camel Case"
+      solution "breakCamelCase" `shouldBe` "break Camel Case"
+    it "should have the correct length for random words" $ do
+      property $ 
+        forAll (listOf1 $ (fmap (\(x:xs) -> toUpper x : xs)) $ listOf1 $ choose ('a', 'z')) $ \xs ->
+        length (words . solution . concat $ xs ) `shouldBe` length xs
