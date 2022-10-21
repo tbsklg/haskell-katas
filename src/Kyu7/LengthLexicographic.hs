f61fe4b587c32c0d88057df32c31fcca925ed8e0
Tobias Klug <tbs.klg@gmail.com>
2022-10-21 09:05:59 +0200

    Solve 542712c3a16825621e000b65: Sorting lists by length and lexicographically

---
+++ src/Kyu7/LengthLexicographic.hs
@@ -0,0 +1,10 @@
+module Kyu7.LengthLexicographic where
+
+newtype LengthList a = LengthList [a]
+ deriving(Show,Eq)
+ 
+instance Ord a => Ord (LengthList a) where
+  compare (LengthList a) (LengthList b)
+    | length a < length b = LT
+    | length a > length b = GT
+    | otherwise = Prelude.compare a b
---
+++ test/Kyu7/LengthLexicographicSpec.hs
@@ -0,0 +1,31 @@
+module Kyu7.LengthLexicographicSpec where
+
+import Kyu7.LengthLexicographic
+import Data.List
+import Test.Hspec
+import Text.Printf
+
+spec :: Spec
+spec = do
+  describe "examples" $ do
+      it (printf "Should return the right comparison results.") 
+      $ do
+        (compare x y) `shouldBe` GT
+        (compare y x) `shouldBe` LT
+        (compare x z) `shouldBe` LT
+        (compare z x) `shouldBe` GT
+        (compare y z) `shouldBe` LT
+        (compare z y) `shouldBe` GT
+        (compare x x) `shouldBe` EQ
+        (compare y y) `shouldBe` EQ
+        (compare z z) `shouldBe` EQ
+  describe "Sorting list of strings" $ do
+    it (printf "should return %s given %s as input" (show stringsExpected) (show strings)) 
+    $ do
+      (sort strings) `shouldBe` stringsExpected
+  where
+    x = LengthList [1,2,3,4,5]
+    y = LengthList [1,2,3,4]
+    z = LengthList [1,2,3,4,6]
+    strings = [LengthList "BORAL", LengthList "BORAS", LengthList "HEBE", LengthList "HECH", LengthList "BORED", LengthList "HEAD", LengthList "HEAL", LengthList "BORAX", LengthList "HEAR", LengthList "HEAT", LengthList "HEAP", LengthList "BORER", LengthList "HECK"]
+    stringsExpected = [LengthList "HEAD",LengthList "HEAL",LengthList "HEAP",LengthList "HEAR",LengthList "HEAT",LengthList "HEBE",LengthList "HECH",LengthList "HECK",LengthList "BORAL",LengthList "BORAS",LengthList "BORAX",LengthList "BORED",LengthList "BORER"]
