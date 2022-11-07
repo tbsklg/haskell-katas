66cf81794b41653f565b937b59b683539ad84e8b
Tobias Klug <tbs.klg@gmail.com>
2022-11-07 08:04:46 +0100

    Solve 5266876b8f4bf2da9b000362: Who likes it?

---
+++ test/Kyu6/LikesSpec.hs
@@ -0,0 +1,13 @@
+module Kyu6.LikesSpec where
+import Test.Hspec
+
+import Kyu6.Likes
+
+spec :: Spec
+spec = do
+  describe "Static tests" $ do
+    it "Static test: empty" $ likes [] `shouldBe` "no one likes this"
+    it "Static test: 1 name" $ likes ["Peter"] `shouldBe` "Peter likes this"
+    it "Static test: 2 names" $ likes ["Jacob", "Alex"] `shouldBe` "Jacob and Alex like this"
+    it "Static test: 3 names" $ likes ["Max", "John", "Mark"] `shouldBe` "Max, John and Mark like this"
+    it "Static test: 4 names" $ likes ["Alex", "Jacob", "Mark", "Max"] `shouldBe` "Alex, Jacob and 2 others like this"
