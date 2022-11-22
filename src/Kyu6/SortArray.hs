b1328ee6a1c1026ce800b609267a7a716073c4fd
Tobias Klug <tbs.klg@gmail.com>
2022-11-22 08:10:10 +0100

    Solve 578aa45ee9fd15ff4600090d: Sort the odd

---
+++ src/Kyu6/SortArray.hs
@@ -0,0 +1,18 @@
+module Kyu6.SortArray where
+
+import Data.List (sortOn, sort)
+
+sortArray :: [Int] -> [Int]
+sortArray xs = map snd . sortOn fst $ (evens ++ sortedOdd)
+  where
+    evens = filter (\(x, y) -> even y) indexed
+    sortedOdd = zip oddIndexes . sort . filter odd $ xs
+    oddIndexes = map fst . filter (\(x, y) -> odd y) $ indexed
+    indexed = zip [0 ..] xs
+
+-- sortArray :: [Int] -> [Int]
+-- sortArray = replaceOdd <$> id <*> sort . filter odd
+--   where replaceOdd xs [] = xs
+--         replaceOdd (x:xs) oos@(o:os)
+--           | even x    = x : replaceOdd xs oos
+--           | otherwise = o : replaceOdd xs os
---
+++ test/Kyu6/SortArraySpec.hs
@@ -0,0 +1,11 @@
+module Kyu6.SortArraySpec where
+import Kyu6.SortArray
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "The sortArray function" $ do
+    it "should work for some examples" $ do
+      sortArray [5, 3, 2, 8, 1, 4] `shouldBe` [1, 3, 2, 8, 5, 4]
+      sortArray [5, 3, 1, 8, 0] `shouldBe` [1, 3, 5, 8, 0]
+      sortArray [] `shouldBe` []
