e2fd14ee46f096066a9f667d315dc474d1554aab
Tobias Klug <tbs.klg@gmail.com>
2023-01-01 08:10:10 +0100

    Solve 57eb8fcdf670e99d9b000272: Highest Scoring Word

---
+++ src/Kyu6/HighestWord.hs
@@ -0,0 +1,18 @@
+module Kyu6.HighestWord where
+
+import Data.List (maximumBy, elemIndex)
+import Data.Function (on)
+import Data.Maybe (fromJust)
+
+high :: String -> String
+high myStr = fst . maximumBy score . map (\x -> (x, calculateScore x)) . words $ myStr
+
+calculateScore word = sum . map (\x -> (+) 1 . fromJust . elemIndex x $ alphabet) $ word
+
+score :: (String, Int) -> (String, Int) -> Ordering
+score (a, sa) (b, sb)
+    | sa < sb = LT
+    | otherwise = GT
+
+alphabet :: String
+alphabet = ['a' .. 'z']
