fa27b8bd1c8b129b0ec7f792b26dc4e3d03fd980
Tobias Klug <tbs.klg@gmail.com>
2023-02-03 06:10:10 +0100

    Solve 55bf01e5a717a0d57e0000ec: Persistent Bugger.

---
+++ test/Kyu6/PersistenceSpec.hs
@@ -0,0 +1,21 @@
+module Kyu6.PersistenceSpec where
+import Kyu6.Persistence (persistence)
+
+import Data.Char
+
+import Test.Hspec
+import Text.Printf (printf)
+        
+testPersistence :: Int -> Int -> Spec
+testPersistence n s = 
+    it (printf "should return persistence for : %d" n) $
+        persistence n `shouldBe` s
+    
+spec :: Spec
+spec = do
+    describe "Persistence: Basic Tests" $ do        
+        testPersistence 39 3
+        testPersistence 4 0
+        testPersistence 25 2
+        testPersistence 999 4
+        
