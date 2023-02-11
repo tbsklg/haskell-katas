5589e46ca3a6def50c3e373e83b59cdf0cf810ab
Tobias Klug <tbs.klg@gmail.com>
2023-02-11 06:10:10 +0100

    Solve 58b57ae2724e3c63df000006: Parse HTML/CSS Colors

---
+++ test/Kyu6/ParseHtmlColorSpec.hs
@@ -0,0 +1,16 @@
+module Kyu6.ParseHtmlColorSpec where
+
+import Kyu6.ParseHtmlColor (parseHtmlColor)
+import Data.Map.Strict (Map, fromList, (!))
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "Example tests" $ do
+    it "should parse a 6-digit color" $
+      parseHtmlColor "#80FFA0" `shouldBe` fromList [('r',128), ('g',255), ('b',160)]
+    it "should parse a 3-digit color" $
+      parseHtmlColor "#3B7" `shouldBe` fromList [('r',51), ('g',187), ('b',119)]
+    it "should parse a preset color" $
+      parseHtmlColor "LimeGreen" `shouldBe` fromList [('r',50), ('g',205), ('b',50)]
