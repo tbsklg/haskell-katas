4ffdf6f657b069ee831e1e73816fdb3eecd6605b
Tobias Klug <tbs.klg@gmail.com>
2023-02-23 07:55:11 +0100

    Solve 55466989aeecab5aac00003e: Rectangle into Squares

--- src/Kyu6/Rectangle.hs
+++ src/Kyu6/Rectangle.hs
@@ -1,5 +1,12 @@
 module Kyu6.Rectangle where
 
 squaresInRect :: Integer -> Integer -> Maybe [Integer]
-squaresInRect lng wdth = error "todo: squaresInRect"
+squaresInRect lng wdth
+  | lng == wdth = Nothing
+  | otherwise = Just $ go lng wdth
+  where
+    go l w
+      | l == w = [l]
+      | l > w = w : go (l - w) w
+      | l < w = l : go l (w - l)
 
