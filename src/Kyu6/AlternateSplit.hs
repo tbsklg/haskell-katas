d90bc3755b39cb031cb7134dbe4e2cda3acd7325
Tobias Klug <tbs.klg@gmail.com>
2023-03-14 08:23:37 +0100

    Solve 57814d79a56c88e3e0000786: Simple Encryption #1 - Alternating Split

---
+++ src/Kyu6/AlternateSplit.hs
@@ -0,0 +1,40 @@
+module Kyu6.AlternateSplit where
+
+import Data.List (sortOn)
+
+encrypt :: String -> Int -> String
+encrypt xs n | n <= 0 = xs
+encrypt xs n = encrypt (odds ++ evens) (n - 1)
+  where
+    odds = map snd . filter (\(i, x) -> odd i) . zip [0 ..] $ xs
+    evens = map snd . filter (\(i, x) -> even i) . zip [0 ..] $ xs
+
+decrypt :: String -> Int -> String
+decrypt xs n | n <= 0 = xs
+decrypt xs n = decrypt decrypted (n - 1)
+  where
+    numberOfOdds = length xs `div` 2
+    numberOfEvens = length xs - numberOfOdds
+
+    oddsWithIndices = zip [1, 3 ..] $ take numberOfOdds xs
+    evensWithIndices = zip [0, 2 ..] $ take numberOfEvens . drop numberOfOdds $ xs
+
+    decrypted = map snd . sortOn fst $ (oddsWithIndices ++ evensWithIndices)
+
+
+-- import Data.List (partition, transpose)
+
+-- encrypt :: String -> Int -> String
+-- encrypt s n 
+--   | n > 0 = iterate go s !! n
+--   | otherwise = s
+--   where
+--     go = map snd . uncurry (++) . partition (odd . fst) . zip [0..]
+    
+-- decrypt :: String -> Int -> String
+-- decrypt s n 
+--   | n > 0 = iterate go s !! n
+--   | otherwise = s
+--   where
+--     l = length s
+--     go s = concat $ transpose $ [drop (l `div` 2) s, take (l `div` 2) s]  
