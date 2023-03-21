fff95954ea0270bffcf9d843df3f28f7218f38f2
Tobias Klug <tbs.klg@gmail.com>
2023-03-21 07:31:03 +0100

    Solve 5a580064e6be38fd34000147: Create Four Letter Birding Codes from Bird Names

---
+++ src/Kyu6/BirdCode.hs
@@ -0,0 +1,15 @@
+module Kyu6.BirdCode where
+
+import Data.List.Split (splitOneOf)
+import Data.Char (toUpper)
+
+birdCode :: [String] -> [String]
+birdCode = map (toUpperCase . createCode . splitOneOf "- ")
+  where
+    toUpperCase = map toUpper
+
+    createCode [x] = take 4 x
+    createCode [x, y] = take 2 x ++ take 2 y
+    createCode [x, y, z] = [head x] ++ [head y] ++ take 2 z
+    createCode bird@[_, _, _, _] = map head bird
+    createCode _ = error "Could not create four letter birding code"
