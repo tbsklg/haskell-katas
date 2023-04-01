749f661e62fc6a4a00ed649ebe6294b5782522fe
Tobias Klug <tbs.klg@gmail.com>
2023-04-01 16:42:50 +0200

    Solve 5526fc09a1bbd946250002dc: Find the Parity Outlier

---
+++ src/Kyu6/FindOutlier.hs
@@ -0,0 +1,16 @@
+module Kyu6.FindOutlier (findOutlier) where
+
+findOutlier :: [Int] -> Int 
+findOutlier xs
+  | length evens == 1 = head evens
+  | otherwise = head odds
+  where
+    evens = filter even xs
+    odds = filter odd xs
+
+-- findOutlier :: [Int] -> Int
+-- findOutlier xs =
+--    case partition even xs of
+--        ([x], _) -> x
+--        (_, [x]) -> x
+--        otherwise -> error "invalid input"
---
+++ test/Kyu6/FindOutlierSpec.hs
@@ -0,0 +1,16 @@
+module Kyu6.FindOutlierSpec (spec) where
+
+import Kyu6.FindOutlier 
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+
+    findOutlier [2, 4, 0, 100, 4, 11, 2602, 36] `shouldBe` 11
+    findOutlier [160, 3, 1719, 19, 11, 13, -21] `shouldBe` 160
+
+    findOutlier [2,6,8,-10,3] `shouldBe` 3
+    findOutlier [206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781] `shouldBe` 206847684
+    findOutlier [2147483647,0,1] `shouldBe` 0
+
