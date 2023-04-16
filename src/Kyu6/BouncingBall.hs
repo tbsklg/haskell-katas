a8f85d5a9c4dd5733e53ccbe515cb82e13ed9835
Tobias Klug <tbs.klg@gmail.com>
2023-04-16 18:41:32 +0200

    Solve 5544c7a5cb454edb3c000047: Bouncing Balls

---
+++ src/Kyu6/BouncingBall.hs
@@ -0,0 +1,10 @@
+module Kyu6.BouncingBall where
+
+bouncingBall :: Double -> Double -> Double -> Integer
+bouncingBall height bounce window
+  | height <= 0 || bounce <= 0 || bounce >= 1 || window >= height = -1
+  | otherwise = 1 + bounce' height bounce window
+ where
+  bounce' h b w
+    | h * b <= w = 0
+    | otherwise = 2 + bounce' (h * b) b w
---
+++ test/Kyu6/BouncingBallSpec.hs
@@ -0,0 +1,11 @@
+module Kyu6.BouncingBallSpec (spec, main) where
+
+import Kyu6.BouncingBall
+import Test.Hspec
+
+main = hspec spec
+spec = do
+  describe "BouncingBall" $ do
+    it "1st series" $ do
+      bouncingBall 3.0 0.66 1.5 `shouldBe` 3
+      bouncingBall 30.0 0.66 1.5 `shouldBe` 15
