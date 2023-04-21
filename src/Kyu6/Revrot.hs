428a20aa848322173b0c9f7b89c96a86c38c8744
Tobias Klug <tbs.klg@gmail.com>
2023-04-21 18:17:44 +0200

    Solve 56b5afb4ed1f6d5fb0000991: Reverse or rotate?

---
+++ src/Kyu6/Revrot.hs
@@ -0,0 +1,16 @@
+module Kyu6.Revrot where
+
+import Data.Char (digitToInt)
+import Data.List.Split (chunksOf)
+
+revRot :: [Char] -> Int -> [Char]
+revRot _ 0 = []
+revRot strng sz = concatMap reverseOrRotate . filter (\x -> length x == sz) . chunksOf sz $ strng
+ where
+  shiftLeft [] = []
+  shiftLeft (x : xs) = xs ++ [x]
+
+  reverseOrRotate :: [Char] -> [Char]
+  reverseOrRotate chunk
+    | even . sum . map ((^ 2) . digitToInt) $ chunk = reverse chunk
+    | otherwise = shiftLeft chunk
---
+++ test/Kyu6/RevrotSpec.hs
@@ -0,0 +1,21 @@
+module Kyu6.RevrotSpec where
+import Kyu6.Revrot (revRot)
+
+import Test.Hspec
+import Test.QuickCheck
+import Text.Printf (printf)
+
+testRevRot :: [Char] -> Int -> [Char] -> Spec
+testRevRot strng sz s = 
+    it (printf "should return revRot for s: %s n %d " strng sz) $
+        revRot strng sz `shouldBe` s
+
+
+spec :: Spec
+spec = do
+    describe "revRot" $ do
+        testRevRot "1234" 0 ""
+        testRevRot "" 5 ""
+        testRevRot "1234" 5 ""
+        testRevRot "733049910872815764" 5 "330479108928157"
+
