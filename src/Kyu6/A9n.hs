18ef7974dd0ab6191ba05b97b2e79f1edbeb1a6e
Tobias Klug <tbs.klg@gmail.com>
2023-05-01 15:48:18 +0200

    Solve 5375f921003bf62192000746: Word a10n (abbreviation)

---
+++ src/Kyu6/A9n.hs
@@ -0,0 +1,17 @@
+module Kyu6.A9n where
+
+import Data.Char (isAlpha)
+import Data.List.Split (splitOn)
+
+abbreviate :: String -> String
+abbreviate = unwords . map abbreviateWord . words'
+ where
+  words' = splitOn " "
+
+  abbreviateWord [] = []
+  abbreviateWord word@(x : xs)
+    | isAlpha x = (abbreviate' . takeWhile isAlpha $ word) ++ (abbreviateWord . dropWhile isAlpha $ word)
+    | otherwise = x : abbreviateWord xs
+
+  abbreviate' xs | length xs < 4 = xs
+  abbreviate' (x : xs) = [x] ++ show (length xs - 1) ++ [last xs]
