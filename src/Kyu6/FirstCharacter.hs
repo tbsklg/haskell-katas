fa57e8ca4b84a0097001c8324824904e346ce328
Tobias Klug <tbs.klg@gmail.com>
2023-05-05 07:55:11 +0200

    Solve 54f9f4d7c41722304e000bbb: first character that repeats

---
+++ src/Kyu6/FirstCharacter.hs
@@ -0,0 +1,7 @@
+module Kyu6.FirstCharacter where
+
+firstDup :: Eq a => [a] -> Maybe a
+firstDup [] = Nothing
+firstDup (x : xs)
+  | x `elem` xs = Just x
+  | otherwise = firstDup xs
---
+++ test/Kyu6/FirstCharacterSpec.hs
@@ -0,0 +1,16 @@
+module Kyu6.FirstCharacterSpec where
+
+import Kyu6.FirstCharacter (firstDup)
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "firstDub" $ do
+    it "should work for some simple tests" $ do
+      firstDup "tweet" `shouldBe` Just 't'
+      firstDup (cycle [1, 2, 3]) `shouldBe` Just 1
+      firstDup (repeat ()) `shouldBe` Just ()
+      firstDup [] `shouldBe` (Nothing :: Maybe Integer)
+      firstDup [1 .. 10] `shouldBe` Nothing
+      firstDup [2, 46, 3, 1, 1, 2] `shouldBe` Just 2
