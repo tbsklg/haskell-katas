b83b29ab14e30d5b1a3a56b88ffb64fc05e3f085
Tobias Klug <tbs.klg@gmail.com>
2023-05-17 07:47:59 +0200

    Solve 5596f6e9529e9ab6fb000014: Calculate String Rotation

---
+++ src/Kyu6/Rotation.hs
@@ -0,0 +1,16 @@
+module Kyu6.Rotation where
+
+import Debug.Trace
+
+shiftedDiff :: String -> String -> Int
+shiftedDiff a b
+  | length a /= length b = -1
+  | otherwise = countShifts a b 0
+  where
+    countShifts a b c
+      | c > length a = -1
+      | a == b = c
+      | otherwise = countShifts a (shift b) (c + 1)
+
+    shift [] = []
+    shift (x:xs) = xs ++ [x]
---
+++ test/Kyu6/RotationSpec.hs
@@ -0,0 +1,28 @@
+module Kyu6.RotationSpec (spec, main) where
+
+import Kyu6.Rotation (shiftedDiff)
+import Control.Monad (forM_)
+import Test.Hspec
+import Test.QuickCheck
+
+main = hspec spec
+spec = describe "shiftedDiff" $ do
+  it "returns 0 for equal strings" $ property $ \(NonEmpty xs) ->
+    shiftedDiff xs xs `shouldBe` 0
+  it "returns -1 for unequal strings" $ property $ \x (NonEmpty xs) ->
+    shiftedDiff (x:xs) xs `shouldBe` (-1)
+    
+  let values = 
+        [ ("coffee", "eecoff", 2)
+        , ("eecoff", "coffee",4)
+        , ("Moose","moose", -1)
+        , ("isn't","'tisn", 2)
+        , ("Esham","Esham", 0)
+        , (" "," ", 0)
+        , ("hoop","pooh", -1)
+        , ("  "," ", -1)
+        ]
+  forM_ values $ \(a,b,e) ->
+    it ("returns " ++ show e ++ " on " ++ show a ++ " " ++ show b) $ 
+      shiftedDiff a b `shouldBe` e
+
