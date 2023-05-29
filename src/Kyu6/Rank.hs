a6627725b1f9c7fff43c8e43ec19f433d5c81b26
Tobias Klug <tbs.klg@gmail.com>
2023-05-29 08:58:11 +0200

    Solve 5616868c81a0f281e500005c: Prize Draw

---
+++ src/Kyu6/Rank.hs
@@ -0,0 +1,23 @@
+module Kyu6.Rank where
+
+import Data.Char (ord, toLower)
+import Data.List (sortBy)
+import Data.List.Split (splitOn)
+import Debug.Trace (traceShow)
+
+rank :: [Char] -> [Int] -> Int -> [Char]
+rank st we n
+  | null st = "No participants"
+  | n > length players = "Not enough participants"
+  | otherwise = traceShow (scores) fst $ scores !! (n - 1)
+ where
+  rank (a, b) (c, d)
+    | b == d = compare a c
+    | b > d = LT
+    | otherwise = GT
+
+  scores = sortBy rank . zip players . zipWith (*) (map som players) $ we
+  players = splitOn "," st
+
+  som x = (+) (length x) . sum . map (alphabetPosition . toLower) $ x
+  alphabetPosition x = ord x - ord 'a' + 1
