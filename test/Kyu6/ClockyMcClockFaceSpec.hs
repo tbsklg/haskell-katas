3692d3ff60bdb249f2b791c3e0ed1617dcdb55a6
Tobias Klug <tbs.klg@gmail.com>
2023-05-30 21:25:30 +0200

    Solve 59752e1f064d1261cb0000ec: Clocky Mc Clock-Face

---
+++ test/Kyu6/ClockyMcClockFaceSpec.hs
@@ -0,0 +1,15 @@
+module Kyu6.ClockyMcClockFaceSpec where
+
+import Kyu6.ClockyMcClockFace (whatTimeIsIt)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "Clocky McClockFace" $ do
+      it "Example Tests" $ do
+        whatTimeIsIt   0 `shouldBe` "12:00"
+        whatTimeIsIt  90 `shouldBe` "03:00"
+        whatTimeIsIt 180 `shouldBe` "06:00"
+        whatTimeIsIt 270 `shouldBe` "09:00"
+        whatTimeIsIt 360 `shouldBe` "12:00"
+
