3fdc5c2134c5808eaef277630dc4c19da5866479
Tobias Klug <tbs.klg@gmail.com>
2023-06-18 12:30:38 +0200

    Solve 54b72c16cd7f5154e9000457: Decode the Morse code, advanced

---
+++ src/Kyu4/DecodeMorseAdvanced.hs
@@ -0,0 +1,40 @@
+module Kyu4.DecodeMorseAdvanced where
+
+import Data.List (group)
+import Data.List.Split (splitOn)
+import Data.Map as M (fromList, (!))
+import Data.Text (dropAround, pack, unpack)
+
+morseCodes = M.fromList [("-", "T"), ("--", "M"), ("---", "O"), ("-----", "0"), ("----.", "9"), ("---..", "8"), ("---...", ":"), ("--.", "G"), ("--.-", "Q"), ("--..", "Z"), ("--..--", ","), ("--...", "7"), ("-.", "N"), ("-.-", "K"), ("-.--", "Y"), ("-.--.", "("), ("-.--.-", ")"), ("-.-.", "C"), ("-.-.--", "!"), ("-.-.-.", ";"), ("-..", "D"), ("-..-", "X"), ("-..-.", "/"), ("-...", "B"), ("-...-", "="), ("-....", "6"), ("-....-", "-"), (".", "E"), (".-", "A"), (".--", "W"), (".---", "J"), (".----", "1"), (".----.", "'"), (".--.", "P"), (".--.-.", "@"), (".-.", "R"), (".-.-.", "+"), (".-.-.-", "."), (".-..", "L"), (".-..-.", "\""), (".-...", "&"), ("..", "I"), ("..-", "U"), ("..---", "2"), ("..--.-", "_"), ("..--..", "?"), ("..-.", "F"), ("...", "S"), ("...-", "V"), ("...--", "3"), ("...---...", "SOS"), ("...-..-", "$"), ("....", "H"), ("....-", "4"), (".....", "5")]
+
+decodeMorse :: String -> String
+decodeMorse = unwords . map decode . words'
+ where
+  words' = splitOn "  "
+  decode = concatMap decoded . words
+  decoded x = morseCodes M.! x
+
+decodeBits :: String -> String
+decodeBits bits = decode (trimZeros bits) (transmissionRate bits)
+ where
+  decode "" _ = ""
+  decode rBits@('1' : _) rate = (dotOrDash rate . takeWhile (== '1') $ rBits) ++ decode (dropWhile (== '1') rBits) rate
+  decode rBits@('0' : _) rate = (pause rate . takeWhile (== '0') $ rBits) ++ decode (dropWhile (== '0') rBits) rate
+  decode _ _ = error "Invalid message"
+
+  dotOrDash rate signal
+    | length signal == rate = "."
+    | length signal == 3 * rate = "-"
+    | otherwise = error "Invalid signal"
+
+  pause rate signal
+    | length signal == rate = ""
+    | length signal == 3 * rate = " "
+    | length signal == 7 * rate = "   "
+    | otherwise = error "Invalid signal"
+
+transmissionRate :: String -> Int
+transmissionRate = minimum . map length . group . trimZeros
+
+trimZeros :: String -> String
+trimZeros = unpack . dropAround (== '0') . pack
---
+++ test/Kyu4/DecodeMorseAdvancedSpec.hs
@@ -0,0 +1,36 @@
+module Kyu4.DecodeMorseAdvancedSpec where
+
+import Kyu4.DecodeMorseAdvanced (decodeBits, decodeMorse, transmissionRate)
+import Test.Hspec
+
+decode = decodeMorse . decodeBits
+
+spec :: Spec
+spec = do
+  describe "Example tests" $ do
+    it "should correctly decode example from description" $ do
+      decode "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" `shouldBe` "HEY JUDE"
+      decode "1" `shouldBe` "E"
+      decode "101" `shouldBe` "I"
+      decode "10001" `shouldBe` "EE"
+      decode "10111" `shouldBe` "A"
+      decode "1110111" `shouldBe` "M"
+      decode "111" `shouldBe` "E"
+      decode "1111111" `shouldBe` "E"
+      decode "110011" `shouldBe` "I"
+      decode "01110" `shouldBe` "E"
+
+  describe "Calculate transmission rate" $ do
+    it "should correctly calculate transmission rate" $ do
+      transmissionRate "1" `shouldBe` 1
+      transmissionRate "111" `shouldBe` 3
+      transmissionRate "111000" `shouldBe` 3
+      transmissionRate "111000111" `shouldBe` 3
+      transmissionRate "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" `shouldBe` 2
+      transmissionRate "10001" `shouldBe` 1
+
+  describe "It should decode bits to dots and dashes" $ do
+    it "should decode bits" $ do
+      decodeBits "1" `shouldBe` "."
+      decodeBits "111" `shouldBe` "."
+      decodeBits "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" `shouldBe` ".... . -.--   .--- ..- -.. ." 
