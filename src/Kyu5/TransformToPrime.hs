b589fc85031cff181292c3c1d3f1636f111cc7db
Tobias Klug <tbs.klg@gmail.com>
2023-06-21 05:58:11 +0200

    Solve 5a946d9fba1bb5135100007c: Transform To Prime

---
+++ src/Kyu5/TransformToPrime.hs
@@ -0,0 +1,8 @@
+module Kyu5.TransformToPrime.Kata where
+
+minimumNumber :: [Integer] -> Integer
+minimumNumber xs = nextPrimeFrom (sum xs) - sum xs
+
+nextPrimeFrom x = head . dropWhile (not . isPrime) $ [x ..]
+ where
+  isPrime n = all (\x -> n `mod` x /= 0) [2 .. (n -1)]
