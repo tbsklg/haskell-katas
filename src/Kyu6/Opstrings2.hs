2eb155bf060a537650037936a7d7ae2c4f58a0cc
Tobias Klug <tbs.klg@gmail.com>
2023-06-27 07:40:40 +0200

    Solve 56dbe7f113c2f63570000b86: Moves in squared strings

---
+++ src/Kyu6/Opstrings2.hs
@@ -0,0 +1,16 @@
+module Kyu6.Opstrings2 where
+
+rot :: String -> String
+rot = trimNewLine . unlines . reverse . map reverse . lines
+
+selfieAndRot :: String -> String
+selfieAndRot xs = trimNewLine $ unlines (print xs ++ printRot xs)
+    where
+        print = map (\x -> x ++ replicate (length x) '.') . lines
+        printRot = map (\x -> replicate (length x) '.' ++ x) . lines . rot
+
+trimNewLine :: String -> String
+trimNewLine = reverse . dropWhile (== '\n') . reverse
+
+oper :: (String -> String) -> String -> String
+oper fct = fct
