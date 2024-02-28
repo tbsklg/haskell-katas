38d166e07cc405607778e0c4292a17d5a17a219b
Tobias Klug <tbs.klg@gmail.com>
2024-02-28 14:09:31 +0100

    Update rot13

--- src/Kyu5/Rot13.hs
+++ src/Kyu5/Rot13.hs
@@ -11,5 +11,5 @@ shift13 = shift 13
 shift :: Int -> Char -> Char
 shift size x
   | isAsciiUpper x = chr . (+) (ord 'A') . mod (ord x - ord 'A' + size) $ 26
-  | isAsciiLower x && isLower x = chr . (+) (ord 'a') . mod (ord x - ord 'a' + size) $ 26
+  | isAsciiLower x = chr . (+) (ord 'a') . mod (ord x - ord 'a' + size) $ 26
   | otherwise = x
