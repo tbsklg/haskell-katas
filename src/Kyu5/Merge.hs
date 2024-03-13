77f05f27c113663657b16f47c755894264423e91
Tobias Klug <tbs.klg@gmail.com>
2024-03-13 07:02:14 +0100

    Refactor merge sort

--- src/Kyu5/Merge.hs
+++ src/Kyu5/Merge.hs
@@ -1,11 +1,9 @@
 module Kyu5.Merge where
 
-import Debug.Trace
-
 merge :: Ord a => [a] -> [a] -> [a]
 merge [] [] = []
 merge [] ys = ys
 merge xs [] = xs
-merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys) else y : merge (x:xs) ys
-
-
+merge l@(x:xs) r@(y:ys)
+  | x < y = x : merge xs r
+  | otherwise = y : merge l ys
