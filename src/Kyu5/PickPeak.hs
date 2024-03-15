f58f336060ab11acaca9e1ba7ce15727dfbf1656
Tobias Klug <tbs.klg@gmail.com>
2024-03-15 06:34:11 +0100

    Solve picked peak

---
+++ src/Kyu5/PickPeak.hs
@@ -0,0 +1,20 @@
+module Kyu5.PickPeak where
+
+data PickedPeaks = PickedPeaks {pos :: [Int], peaks :: [Int]} deriving (Eq, Show)
+
+pickPeaks :: [Int] -> PickedPeaks
+pickPeaks = go [] [] . clean . zip [0 ..]
+ where
+  go pos peaks ((i, x) : (j, y) : (k, z) : xs)
+    | x < y && y >= z = go (pos ++ [j]) (peaks ++ [y]) further
+    | otherwise = go pos peaks further
+   where
+    further = (j, y) : (k, z) : xs
+  go pos peaks _ = PickedPeaks pos peaks
+
+clean :: [(Int, Int)] -> [(Int, Int)]
+clean [] = []
+clean l@((i, x) : (j, y) : xs)
+  | x == y = (i, x) : clean (dropWhile (\(_, x') -> x == x') . tail $ l)
+  | otherwise = (i, x) : clean ((j, y) : xs)
+clean xs = xs
