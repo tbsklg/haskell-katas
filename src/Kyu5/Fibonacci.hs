3940c8bbd9115f274104aed7279c8c16dc8e00b1
Tobias Klug <tbs.klg@gmail.com>
2024-03-21 08:00:02 +0100

    Solve memoized fibnoacci

---
+++ src/Kyu5/Fibonacci.hs
@@ -0,0 +1,6 @@
+module Kyu5.Fibonacci where
+
+fibonacci :: Int -> Integer
+fibonacci n = (xs !! (n - 1)) + (xs !! (n - 2))
+ where
+  xs = 0 : 1 : map fibonacci [2 ..]
---
+++ test/Kyu5/FibonacciSpec.hs
@@ -0,0 +1,16 @@
+module Kyu5.FibonacciSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Kyu5.Fibonacci
+
+spec :: Spec
+spec = do
+  it "Fixed tests" $ do
+      fibonacci 70 `shouldBe` 190392490709135
+      fibonacci 60 `shouldBe` 1548008755920
+      fibonacci 50 `shouldBe` 12586269025
+      
+
+-- the following line is optional for 8.2
+main = hspec spec
