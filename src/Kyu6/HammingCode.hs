f7fb4092d85fe09db751e25ee168971119cae76f
Tobias Klug <tbs.klg@gmail.com>
2024-04-22 07:45:05 +0200

    Update 5ef9ca8b76be6d001d5e1c3e

--- src/Kyu6/HammingCode.hs
+++ src/Kyu6/HammingCode.hs
@@ -1,34 +1,23 @@
 module Kyu6.HammingCode (decode, encode) where
 
-import Data.Char (digitToInt, ord, chr)
+import Data.Char (digitToInt, chr)
 import Data.List.Split (chunksOf)
+import Data.List (sort)
+import Text.Printf (printf)
 
 decode, encode :: [Char] -> [Char]
-encode = concatMap show . concatMap (tripleBit . eightBit . toBin . ord)
+encode = concatMap (replicate 3) . toEightBitBin
 decode = map (chr . toDec) . chunksOf 8 . map correctBits . triples
 
 triples :: [Char] -> [[Int]]
 triples = map (map digitToInt) . chunksOf 3
 
 correctBits :: [Int] -> Int
-correctBits [0, 0, 1] = 0
-correctBits [0, 1, 0] = 0
-correctBits [1, 0, 0] = 0
-correctBits [0, 0, 0] = 0
-correctBits _ = 1
+correctBits = (!! 1) . sort
 
 toDec :: [Int] -> Int
 toDec = foldl (\acc curr -> acc * 2 + curr) 0
 
-tripleBit :: [Int] -> [Int]
-tripleBit = foldr (\curr acc -> [curr, curr, curr] ++ acc) []
-
-eightBit :: [Int] -> [Int]
-eightBit xs =
-  let zeros = replicate (8 - length xs) 0
-   in zeros ++ xs
-
-toBin :: Int -> [Int]
-toBin 0 = []
-toBin x = toBin (x `div` 2) ++ [x `mod` 2]
+toEightBitBin:: [Char] -> [Char]
+toEightBitBin = concatMap (printf "%08b")
 
