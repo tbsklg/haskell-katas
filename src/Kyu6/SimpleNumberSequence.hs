f746f0a19da0ce4a8e7095fe2d5dd1639ddb2a85
Tobias Klug <tbs.klg@gmail.com>
2024-04-25 17:49:10 +0200

    Solve 5a28cf591f7f7019a80000de

---
+++ src/Kyu6/SimpleNumberSequence.hs
@@ -0,0 +1,39 @@
+module Kyu6.SimpleNumberSequence (missing) where
+
+import Data.List (find)
+
+type Gap = Int
+
+missing :: String -> Maybe Int
+missing [] = Nothing
+missing [_, _] = Nothing
+missing xs = findMissing 1
+  where
+    findMissing size | size > length xs `div` 2 = Nothing
+      | isRelevant . extractInts size $ xs = case findGap . extractInts size $ xs of
+          Nothing -> findMissing (size + 1)
+          Just g -> Just g
+      | otherwise = findMissing (size + 1)
+
+isRelevant :: [Int] -> Bool
+isRelevant xs = (gapsWithOne == length xs - 2) && gapsWithTwo == 1
+  where
+    gapsWithOne = length . filter (== 1) $ gaps
+    gapsWithTwo = length . filter (== 2) $ gaps
+    gaps = map (\(x, y) -> y - x) . zip xs . tail $ xs
+
+extractInts :: Gap -> String -> [Int]
+extractInts _ [] = []
+extractInts g xs
+  | extractedNumber == (read . replicate g $ '9') = 
+    extractedNumber : extractInts (g + 1) furtherNumber
+  | otherwise = extractedNumber : extractInts g furtherNumber
+  where
+    extractedNumber = read (take g xs) :: Int
+    furtherNumber = drop g xs
+
+findGap :: [Int] -> Maybe Int
+findGap xs = case find (\(x, y) -> y - x /= 1) . zip xs . tail $ xs of
+  Nothing -> Nothing
+  Just (x, _) -> Just (x + 1)
+
---
+++ test/Kyu6/SimpleNumberSequenceSpec.hs
@@ -0,0 +1,17 @@
+module Kyu6.SimpleNumberSequenceSpec (spec) where
+
+import Test.Hspec
+import Kyu6.SimpleNumberSequence (missing)
+
+spec = do
+  describe "Simple number sequence" $ do
+    it "Example tests" $ do
+      missing "123567" `shouldBe` Just 4
+      missing "899091939495" `shouldBe` Just 92
+      missing "9899101102" `shouldBe` Just 100
+      missing "599600601602" `shouldBe` Nothing
+      missing "8990919395" `shouldBe` Nothing
+      missing "998999100010011003" `shouldBe` Just 1002
+      missing "99991000110002" `shouldBe` Just 10000
+      missing "979899100101102" `shouldBe` Nothing
+      missing "900001900002900004900005900006" `shouldBe` Just 900003
