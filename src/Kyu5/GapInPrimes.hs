75656207f3faba6bf889e33b75c6a9388555ad6c
Tobias Klug <tbs.klg@gmail.com>
2024-04-26 08:08:11 +0200

    Update 561e9c843a2ef5a40c0000a4

--- src/Kyu5/GapInPrimes.hs
+++ src/Kyu5/GapInPrimes.hs
@@ -3,7 +3,14 @@ module Kyu5.GapInPrimes where
 import Data.List (find)
 import Data.Maybe (fromMaybe)
 
-gap :: Integer -> Integer -> Integer -> Maybe (Integer, Integer)
+type Gap = Integer
+type From = Integer
+type To = Integer
+
+type Primes = [Integer]
+type Prime = Integer
+
+gap :: Gap -> From -> To -> Maybe (Prime, Prime)
 gap g m n = find gap' $ zip primes' (fromMaybe [] $ tailMay primes')
   where
     gap' (x, y) = y - x == g
@@ -13,6 +20,6 @@ tailMay :: [a] -> Maybe [a]
 tailMay [] = Nothing
 tailMay (_:xs) = Just xs
 
-primes :: Integer -> Integer -> [Integer]
+primes :: From -> To -> Primes
 primes m n = [x | x <- [m .. n], all (\y -> x `mod` y /= 0) [2 .. floor . sqrt . fromIntegral $ x]]
 
