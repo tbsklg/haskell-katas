6570b462248db35a5f78ce7ac1a048ac067a81e8
Tobias Klug <tbs.klg@gmail.com>
2024-04-26 13:33:02 +0200

    Update 52e88b39ffb6ac53a400022e

--- src/Kyu5/IPv4.hs
+++ src/Kyu5/IPv4.hs
@@ -3,10 +3,9 @@ module Kyu5.IPv4 where
 import Data.Word  (Word32)
 import Data.List.Split (chunksOf)
 import Data.List (intercalate)
-import Data.Bits
 
 type IPString = String
-type Bits' = [Int]
+type Bits = [Int]
 type Bit32 = [Int]
 type Bit8 = [Int]
 
@@ -21,10 +20,10 @@ toDec = foldl (\acc x -> acc * 2 + x) 0
 toOctets :: Bit32 -> Octets
 toOctets = chunksOf 8
 
-toBit32 :: Bits' -> Bit32
+toBit32 :: Bits -> Bit32
 toBit32 xs = replicate (32 - length xs) 0 ++ xs
 
-binary :: Word32 -> Bits'
+binary :: Word32 -> Bits
 binary 0 = []
 binary x = binary (x `div` 2) ++ [fromIntegral x `mod` 2]
 
