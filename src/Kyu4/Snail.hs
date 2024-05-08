6b862753f396dbf7f3a16af06bdfd88deb155c5e
Tobias Klug <tbs.klg@gmail.com>
2024-05-08 17:27:10 +0200

    Format code using ormolu

--- src/Kyu4/Snail.hs
+++ src/Kyu4/Snail.hs
@@ -6,15 +6,16 @@ type Position = (Int, Int)
 
 type Dimension = Int
 
-data Direction = R | L | U | D deriving Show
+data Direction = R | L | U | D deriving (Show)
 
 snail :: [[Int]] -> [Int]
 snail xs = map (\(x, y) -> xs !! x !! y) . reverse . path $ (length xs - 1)
 
 path :: Dimension -> [Position]
-path x = foldl nextPositions [(0,0)]
-  . zip (x: concat [[y, y] | y <- [x, x - 1 .. 0]])
-  $ cycle [R, D, L, U]
+path x =
+  foldl nextPositions [(0, 0)]
+    . zip (x : concat [[y, y] | y <- [x, x - 1 .. 0]])
+    $ cycle [R, D, L, U]
 
 nextPositions :: [Position] -> (Int, Direction) -> [Position]
 nextPositions xs (d, R) = map (right $ currentPosition xs) [d, d - 1 .. 1] ++ xs
@@ -23,7 +24,7 @@ nextPositions xs (d, L) = map (left $ currentPosition xs) [d, d - 1 .. 1] ++ xs
 nextPositions xs (d, U) = map (up $ currentPosition xs) [d, d - 1 .. 1] ++ xs
 
 currentPosition :: [Position] -> Position
-currentPosition = fromMaybe (0,0) . headMay
+currentPosition = fromMaybe (0, 0) . headMay
 
 down :: Position -> Int -> Position
 down (x, y) z = (x + z, y)
--- test/Kyu4/SnailSpec.hs
+++ test/Kyu4/SnailSpec.hs
@@ -6,15 +6,19 @@ import Test.Hspec
 spec :: Spec
 spec = do
   it "First example" $ do
-    let array = [[1,2,3],
-                 [4,5,6],
-                 [7,8,9]]
-        expected = [1,2,3,6,9,8,7,4,5]
+    let array =
+          [ [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+          ]
+        expected = [1, 2, 3, 6, 9, 8, 7, 4, 5]
     snail array `shouldBe` expected
 
   it "Second example" $ do
-    let array = [[1,2,3],
-                 [8,9,4],
-                 [7,6,5]]
-        expected = [1,2,3,4,5,6,7,8,9]
+    let array =
+          [ [1, 2, 3],
+            [8, 9, 4],
+            [7, 6, 5]
+          ]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     snail array `shouldBe` expected
