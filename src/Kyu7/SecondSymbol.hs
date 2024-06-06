919e091566c92ebf14a8505a3250caa6444ad74a
Tobias Klug <tbs.klg@gmail.com>
2024-06-06 06:34:11 +0200

    Solve 63f96036b15a210058300ca9

---
+++ src/Kyu7/SecondSymbol.hs
@@ -0,0 +1,9 @@
+module Kyu7.SecondSymbol (secondSymbol) where
+
+import Data.List (elemIndices)
+
+secondSymbol  :: String -> Char -> Int
+secondSymbol xs c = case elemIndices c xs of
+  [] -> -1
+  [_] -> -1
+  (x:xs) -> head xs
---
+++ test/Kyu7/SecondSymbolSpec.hs
@@ -0,0 +1,16 @@
+module Kyu7.SecondSymbolSpec where
+
+import Test.QuickCheck
+import Test.Hspec
+import Test.HUnit (assertEqual)
+import Kyu7.SecondSymbol (secondSymbol)
+
+spec :: Spec
+spec = do
+  describe "Fixed Tests" $ do
+    it "Basic Test Cases" $ do
+      assertEqual "Find the index of the second symbol 'l' in the string" 3 $ secondSymbol "Hello world!!!" 'l'
+      assertEqual "Find the index of the second symbol 'o' in the string" 7 $ secondSymbol "Hello world!!!" 'o'
+      assertEqual "Find the index of the second symbol 'A' in the string" (-1) $ secondSymbol "Hello world!!!" 'A'
+      assertEqual "The symbol 'q' is not in the string" (-1) $ secondSymbol "" 'q'
+      assertEqual "The symbol '!' is not in the string" (-1) $ secondSymbol "Hello" '!'
