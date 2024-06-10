919c1f5a40f94f959b831af75b24f59db5a8d9cb
Tobias Klug <tbs.klg@gmail.com>
2024-06-10 06:34:11 +0200

    Solve 56b97b776ffcea598a0006f2

---
+++ src/Kyu7/BubbleSortOnce.hs
@@ -0,0 +1,8 @@
+module Kyu7.BubblesortOnce (bubblesortOnce) where
+
+bubblesortOnce :: [Int] -> [Int]
+bubblesortOnce [] = []
+bubblesortOnce [x] = [x]
+bubblesortOnce (x : y : xs)
+  | x > y = y : bubblesortOnce (x : xs) 
+  | otherwise = y : bubblesortOnce (x : xs)
---
+++ test/Kyu7/BubblesortOnceSpec.hs
@@ -0,0 +1,9 @@
+module Kyu7.BubblesortOnceSpec (spec) where
+
+import Kyu7.BubblesortOnce (bubblesortOnce)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    bubblesortOnce [9,7,5,3,1,2,4,6,8] `shouldBe` [7,5,3,1,2,4,6,8,9]
