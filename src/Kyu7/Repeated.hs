2bd33365323843e3e1da5f9ea57e79e80f9e8b2c
Tobias Klug <tbs.klg@gmail.com>
2024-06-16 06:34:11 +0200

    Solve 5ee8ba31b44cc30032cbce04

--- src/Kyu7/Repeated.hs
+++ src/Kyu7/Repeated.hs
@@ -3,4 +3,4 @@ module Kyu7.Repeated (isConsecutive) where
 import Data.List (group, nub)
 
 isConsecutive :: String -> Bool
-isConsecutive xs = nub xs == map head (group xs)
+isConsecutive xs = map head (group xs) == nub xs
