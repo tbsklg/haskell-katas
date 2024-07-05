e9647aebc1e6edd4e16ca80fc3904a04ab34f650
Tobias Klug <tbs.klg@gmail.com>
2024-07-05 09:09:14 +0200

    Solve 5917fbed9f4056205a00001e

---
+++ src/Kyu5/Bananas.hs
@@ -0,0 +1,14 @@
+module Kyu5.Bananas (bananas) where
+
+banana = "banana"
+
+bananas :: String -> [String]
+bananas [] = []
+bananas xs = go xs banana
+  where
+    go xs [] = ['-' <$ xs]
+    go [] _ = []
+    go (x : xs) (b : bs) = 
+      [x : rest | x == b, rest <- go xs bs] 
+        ++ ['-' : rest | rest <- go xs (b : bs)]
+
