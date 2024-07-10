b4437edf8f50ab9270dc4d8d98b5c570e325ecf6
Tobias Klug <tbs.klg@gmail.com>
2024-07-10 13:35:06 +0200

    Refactor 526989a41034285187000de4

--- src/Kyu5/IpsBetween.hs
+++ src/Kyu5/IpsBetween.hs
@@ -1,5 +1,6 @@
 module Kyu5.IpsBetween where
 
+import Data.Bits
 import Data.List.Split (splitOn)
 
 type Ipv4 = String
@@ -8,4 +9,4 @@ ipsBetween :: Ipv4 -> Ipv4 -> Int
 ipsBetween from to = to32Bit to - to32Bit from
 
 to32Bit :: Ipv4 -> Int
-to32Bit ip = foldl (\acc x -> acc * 256 + x) 0 $ map read $ splitOn "." ip
+to32Bit ip = foldl (\acc x -> acc `shiftL` 8 .|. x) 0 $ map read $ splitOn "." ip
